ae3dd73cda554e709a0b8b512f05b839120ef6d6
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-01-18 13:25:26 +0200

    [ELTA-134] Clean code from test feature

--- data/src/main/java/com/elta/android/data/di/RepoModule.kt
+++ data/src/main/java/com/elta/android/data/di/RepoModule.kt
@@ -1,16 +1,7 @@
 package com.elta.android.data.di
 
-import com.elta.android.data.features.feature1.repository.TestDataRepository
-import com.elta.android.domain.features.feature1.repository.TestRepository
-import dagger.Binds
 import dagger.Module
-import javax.inject.Singleton
 
 @Module
 @Suppress("TooManyFunctions")
-abstract class RepoModule {
-
-    @Binds
-    @Singleton
-    abstract fun bindTestRepository(repo: TestDataRepository): TestRepository
-}
+abstract class RepoModule
