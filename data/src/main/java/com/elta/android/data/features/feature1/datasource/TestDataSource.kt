addb0d4d96f2cfef437a26f72c2311ce04e28b64
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-01-18 13:18:54 +0200

    [ELTA-134] Remove test features

--- data/src/main/java/com/elta/android/data/features/feature1/datasource/TestDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.elta.android.data.features.feature1.datasource
-
-import com.elta.android.data.features.feature1.dto.TestDto
-import io.reactivex.Observable
-
-interface TestDataSource {
-
-    fun getTestModel(): Observable<TestDto>
-}
--- domain/src/main/java/com/elta/android/domain/features/feature1/model/TestModel.kt
+++
@@ -1,3 +0,0 @@
-package com.elta.android.domain.features.feature1.model
-
-data class TestModel(val id: String)
