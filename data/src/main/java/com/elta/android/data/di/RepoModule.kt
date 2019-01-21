3f29f2ac0d381a08887dbbaf788de7af37ed0c9d
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-01-21 12:31:44 +0200

    [ELTA-134] Add data implementation of AuthRepository

--- data/src/main/java/com/elta/android/data/di/RepoModule.kt
+++ data/src/main/java/com/elta/android/data/di/RepoModule.kt
@@ -1,7 +1,16 @@
 package com.elta.android.data.di
 
+import com.elta.android.data.features.auth.repository.AuthDataRepository
+import com.elta.android.domain.features.auth.repository.AuthRepository
+import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 @Suppress("TooManyFunctions")
-abstract class RepoModule
+abstract class RepoModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(repo: AuthDataRepository): AuthRepository
+}
