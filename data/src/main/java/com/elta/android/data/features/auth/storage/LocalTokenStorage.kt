c45e6b8e6089691a8137c8395adcd1fe9b1ac8ad
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-01-21 11:35:12 +0200

    [ELTA-133] Make token storage secure

--- data/src/main/java/com/elta/android/data/features/auth/storage/LocalTokenStorage.kt
+++ data/src/main/java/com/elta/android/data/features/auth/storage/LocalTokenStorage.kt
@@ -1,25 +1,24 @@
 package com.elta.android.data.features.auth.storage
 
-import android.content.SharedPreferences
-import com.elta.android.common.utils.setNow
+import com.nullgr.core.security.prefs.CryptoPreferences
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class LocalTokenStorage @Inject constructor(
-    private val pref: SharedPreferences
+    private val pref: CryptoPreferences
 ) : TokenStorage {
 
     override var accessToken: String?
         get() = pref.getString(ACCESS_TOKEN, null)
         set(value) {
-            pref.setNow(ACCESS_TOKEN, value)
+            pref.setString(ACCESS_TOKEN, value)
         }
 
     override var refreshToken: String?
         get() = pref.getString(REFRESH_TOKEN, null)
         set(value) {
-            pref.setNow(REFRESH_TOKEN, value)
+            pref.setString(REFRESH_TOKEN, value)
         }
 
     private companion object {
