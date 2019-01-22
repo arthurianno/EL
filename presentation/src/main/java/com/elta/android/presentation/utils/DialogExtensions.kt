f0edb52ae2e059ba735074add2263b8e87655bc4
Nikita G <grishko188@gmail.com>
2019-01-22 11:53:31 +0200

    [ELTA-180] Implement ProgressDialog

--- presentation/src/main/java/com/elta/android/presentation/utils/DialogExtensions.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/DialogExtensions.kt
@@ -2,12 +2,22 @@
 
 package com.elta.android.presentation.utils
 
+import android.support.v4.app.FragmentManager
 import com.afollestad.materialdialogs.MaterialDialog
+import com.elta.android.presentation.widgets.dialogs.ProgressDialog
+import com.nullgr.core.ui.fragments.showDialog
 import io.reactivex.functions.Consumer
 
 inline fun MaterialDialog.visibility(): Consumer<in Boolean> = Consumer {
     when (it) {
         true -> show()
         else -> dismiss()
+    }
+}
+
+inline fun ProgressDialog.visibility(fragmentManager: FragmentManager): Consumer<in Boolean> = Consumer {
+    when (it) {
+        true -> fragmentManager.showDialog(this)
+        else -> dismissAllowingStateLoss()
     }
 }
