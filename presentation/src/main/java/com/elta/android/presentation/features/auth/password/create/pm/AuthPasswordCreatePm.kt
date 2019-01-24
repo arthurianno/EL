834d751c124e45d3a49e282f864c62dad1c9a7cd
Nikita G <grishko188@gmail.com>
2019-01-24 16:21:30 +0200

    [ELTA-114] Implement password create screen flow

--- presentation/src/main/java/com/elta/android/presentation/features/auth/password/create/pm/AuthPasswordCreatePm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/auth/password/create/pm/AuthPasswordCreatePm.kt
@@ -1,9 +1,73 @@
 package com.elta.android.presentation.features.auth.password.create.pm
 
+import com.elta.android.domain.features.auth.interactor.ResetPasswordUseCase
+import com.elta.android.domain.features.auth.interactor.isPasswordValid
+import com.elta.android.presentation.R
 import com.elta.android.presentation.core.pm.BasePm
 import com.elta.android.presentation.core.pm.ServiceFacade
+import me.dmdev.rxpm.widget.inputControl
+import timber.log.Timber
 import javax.inject.Inject
 
 class AuthPasswordCreatePm @Inject constructor(
+    private val resetPasswordUseCase: ResetPasswordUseCase,
     services: ServiceFacade
-) : BasePm(services)
+) : BasePm(services) {
+
+    val passwordInput = inputControl(hideErrorOnUserInput = false)
+    val saveButtonEnabledState = State(false)
+    val savePasswordAction = Action<Unit>()
+
+    private val token = State<String>()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        passwordInput.text.observable
+            .map(::validatePassword)
+            .map(::getPasswordError)
+            .subscribe(passwordInput.error.consumer)
+            .untilDestroy()
+
+        passwordInput.text.observable
+            .map(::isPasswordValid)
+            .subscribe(saveButtonEnabledState.consumer)
+            .untilDestroy()
+
+        savePasswordAction.observable
+            .skipWhileInProgress()
+            .map(::createResetPasswordParams)
+            .flatMapCompletable { params ->
+                resetPasswordUseCase.execute(params)
+                    .bindProgress()
+                    .doOnComplete(::handleSuccess)
+                    .doOnError(::handleError)
+            }
+            .retry()
+            .subscribe()
+            .untilDestroy()
+    }
+
+    fun passToken(token: String) {
+        this.token.consumer.accept(token)
+    }
+
+    private fun validatePassword(password: String): Boolean =
+        when (password.isEmpty()) {
+            true -> true
+            else -> isPasswordValid(password)
+        }
+
+    private fun getPasswordError(isEmailValid: Boolean): String =
+        when (isEmailValid) {
+            true -> ""
+            else -> resources.getString(R.string.registration_error_input_password)
+        }
+
+    private fun createResetPasswordParams(i: Unit): ResetPasswordUseCase.Params =
+        ResetPasswordUseCase.Params(token.value, passwordInput.text.value)
+
+    private fun handleSuccess() {
+        Timber.d("handleSuccess for Password reset")
+    }
+}
--- presentation/src/main/java/com/elta/android/presentation/features/auth/password/create/ui/AuthPasswordCreateFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/auth/password/create/ui/AuthPasswordCreateFragment.kt
@@ -2,11 +2,17 @@ package com.elta.android.presentation.features.auth.password.create.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import com.elta.android.presentation.R
 import com.elta.android.presentation.core.ui.fragment.BaseFragment
 import com.elta.android.presentation.core.ui.system_ui.LightStatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.features.auth.password.create.pm.AuthPasswordCreatePm
+import com.elta.android.presentation.utils.error
+import com.elta.android.presentation.utils.toggleSecure
+import com.elta.android.presentation.utils.visibility
+import com.jakewharton.rxbinding2.view.clicks
+import kotlinx.android.synthetic.main.fragment_auth_password_create.*
 import kotlinx.android.synthetic.main.layout_auth_toolbar.*
 
 class AuthPasswordCreateFragment : BaseFragment<AuthPasswordCreatePm>() {
@@ -15,9 +21,35 @@ class AuthPasswordCreateFragment : BaseFragment<AuthPasswordCreatePm>() {
     override val classToken: Class<AuthPasswordCreatePm> = AuthPasswordCreatePm::class.java
     override val statusBarConfigProvider: StatusBarConfigProvider = LightStatusBarConfigProvider
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        presentationModel.passToken("Sometoken") // TODO here will be token from deep link
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         homeButtonView.setImageResource(R.drawable.ic_dialog_close)
+    }
+
+    override fun onBindPresentationModel(pm: AuthPasswordCreatePm) {
+        super.onBindPresentationModel(pm)
+        passwordVisibilityButtonView.clicks().bindTo {
+            passwordVisibilityButtonView.toggleSecureIcon(passwordInputView.toggleSecure())
+        }
+        pm.progressState.bindTo(progressDialog.visibility(childFragmentManager))
+        pm.passwordInput.bindTo(passwordInputView)
+        pm.passwordInput.error.observable
+            .distinctUntilChanged()
+            .bindTo(passwordInputView.error())
+        pm.saveButtonEnabledState.bindTo { saveButtonView.isEnabled = it }
+        saveButtonView.clicks().bindTo(pm.savePasswordAction)
+    }
+
+    private fun ImageView.toggleSecureIcon(isSecure: Boolean) {
+        setImageResource(when (isSecure) {
+            true -> R.drawable.ic_show_password
+            else -> R.drawable.ic_password_hide
+        })
     }
 
     companion object {
