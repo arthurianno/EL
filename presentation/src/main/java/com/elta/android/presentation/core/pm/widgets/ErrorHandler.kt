ebccd1deb23b579e3652833ea8085f823a65e07e
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-01-30 14:20:10 +0200

    [ELTA-209] Improve error routing

--- presentation/src/main/java/com/elta/android/presentation/core/pm/widgets/ErrorHandler.kt
+++ presentation/src/main/java/com/elta/android/presentation/core/pm/widgets/ErrorHandler.kt
@@ -5,6 +5,7 @@ import com.elta.android.common.errors.ServiceUnavailableError
 import com.elta.android.presentation.R
 import com.elta.android.presentation.Screens
 import com.elta.android.presentation.States
+import com.elta.android.presentation.core.navigation.FlowRouter
 import com.elta.android.presentation.core.pm.BasePm
 import com.elta.android.presentation.messages.SnackBarMessageData
 import java.net.ConnectException
@@ -15,7 +16,14 @@ class ErrorHandler(private val pm: BasePm) {
 
     fun handleError(error: Throwable) {
         when (error) {
-            is InvalidRefreshTokenError -> pm.flowRouter?.newRootFlow(Screens.Login)
+            is InvalidRefreshTokenError -> {
+                val r = pm.router
+                if (r is FlowRouter) {
+                    r.newRootFlow(Screens.AuthFlow)
+                } else {
+                    r.newRootScreen(Screens.AuthFlow)
+                }
+            }
             else ->
                 if (pm.isEmptyScreen) {
                     if (error.isServerUnavailableError()) {
