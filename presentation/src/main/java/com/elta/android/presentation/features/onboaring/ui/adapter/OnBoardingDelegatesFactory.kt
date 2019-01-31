fd3e9ff9e6d04f78b5924d767b751f8dd676bb8a
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-01-31 09:55:31 +0200

    [ELTA-248] Add OnBoardingGenderItem

---
+++ presentation/src/main/java/com/elta/android/presentation/features/onboaring/ui/adapter/OnBoardingDelegatesFactory.kt
@@ -0,0 +1,20 @@
+package com.elta.android.presentation.features.onboaring.ui.adapter
+
+import com.elta.android.presentation.features.onboaring.ui.adapter.delegates.OnBoardingGenderDelegate
+import com.elta.android.presentation.features.onboaring.ui.adapter.items.OnBoardingGenderItem
+import com.nullgr.core.adapter.AdapterDelegate
+import com.nullgr.core.adapter.AdapterDelegatesFactory
+import com.nullgr.core.adapter.items.ListItem
+import com.nullgr.core.rx.RxBus
+import javax.inject.Inject
+
+class OnBoardingDelegatesFactory @Inject constructor(
+    private val bus: RxBus
+) : AdapterDelegatesFactory {
+
+    override fun createDelegate(clazz: Class<ListItem>): AdapterDelegate =
+        when (clazz) {
+            OnBoardingGenderItem::class.java -> OnBoardingGenderDelegate(bus)
+            else -> throw IllegalArgumentException("No delegate defined for ${clazz.simpleName}")
+        }
+}
