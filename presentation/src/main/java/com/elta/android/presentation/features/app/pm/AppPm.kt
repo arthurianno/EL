5895709c7bf5e947642389e4c558fa5351fa45b7
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-02-05 11:30:35 +0200

    [ELTA-251] Revert start screen

--- presentation/src/main/java/com/elta/android/presentation/features/app/pm/AppPm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/app/pm/AppPm.kt
@@ -20,7 +20,7 @@ class AppPm @Inject constructor(
         super.onCreate()
 
         coldStartAction.observable
-            .doOnNext { router.newRootScreen(Screens.OnBoardingFlow) }
+            .doOnNext { router.newRootScreen(Screens.GreetingFlow) }
             .retry()
             .subscribe()
             .untilDestroy()
