e66b02307488752a35dd15bfde11c48133e944da
Nikita G <grishko188@gmail.com>
2019-02-18 12:52:01 +0200

    [ELTA-380] Add event items for home screen bottom sheet

--- presentation/src/main/java/com/elta/android/presentation/features/home/ui/HomeFlowFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/home/ui/HomeFlowFragment.kt
@@ -1,22 +1,30 @@
 package com.elta.android.presentation.features.home.ui
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
 import android.view.View
 import com.elta.android.presentation.R
 import com.elta.android.presentation.core.ui.fragment.BaseFlowFragment
 import com.elta.android.presentation.features.home.pm.HomeFlowPm
+import com.nullgr.core.adapter.DynamicAdapter
 import kotlinx.android.synthetic.main.fragment_home_flow.*
+import kotlinx.android.synthetic.main.layout_home_bottom_sheet.*
+import javax.inject.Inject
 
 class HomeFlowFragment : BaseFlowFragment<HomeFlowPm>() {
 
     override val screenLayout: Int = R.layout.fragment_home_flow
     override val classToken: Class<HomeFlowPm> = HomeFlowPm::class.java
 
+    @Inject
+    lateinit var adapter: DynamicAdapter
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initBottomSheetItemsView()
         homeBottomNavigationView.select(R.id.mainMenuItemView)
         homePulseView.start()
-        homeBottomSheetView.visibilityChanges().subscribe { visible ->
+        homeBottomSheetView.visibilityChanges().bindTo { visible ->
             homeActionView.isSelected = visible
         }
         homeActionView.setOnClickListener {
@@ -28,10 +36,20 @@ class HomeFlowFragment : BaseFlowFragment<HomeFlowPm>() {
         }
     }
 
+    override fun onBindPresentationModel(pm: HomeFlowPm) {
+        super.onBindPresentationModel(pm)
+        pm.bottomSheetItems.bindTo { items -> adapter.updateData(items) }
+    }
+
     override fun handleBack() {
         if (!homeBottomSheetView.handleBack()) {
             super.handleBack()
         }
+    }
+
+    private fun initBottomSheetItemsView() {
+        bottomSheetItemsView.layoutManager = LinearLayoutManager(activity)
+        bottomSheetItemsView.adapter = adapter
     }
 
     companion object {
---
+++ presentation/src/main/java/com/elta/android/presentation/features/home/ui/adapter/items/EventItem.kt
@@ -0,0 +1,8 @@
+package com.elta.android.presentation.features.home.ui.adapter.items
+
+import com.nullgr.core.adapter.items.ListItem
+
+data class EventItem(
+    val iconRes: Int,
+    val titleRes: Int
+) : ListItem
