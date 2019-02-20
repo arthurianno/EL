eef5612bd31b4da92572bf7967351725478d91c5
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-02-20 12:49:34 +0200

    [CodeStyle] Fix code style issues

--- presentation/src/main/java/com/elta/android/presentation/utils/DateTimeExtensions.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/DateTimeExtensions.kt
@@ -7,11 +7,11 @@ import java.util.Calendar
 fun Calendar.getGreetingText(resource: ResourceProvider): String =
     resource.getString(
         when (this.get(Calendar.HOUR_OF_DAY)) {
-            in MORNING_START_TIME..(MORNING_END_TIME - 1) ->
+            in MORNING_START_TIME until MORNING_END_TIME ->
                 R.string.main_records_new_day_title_morning
-            in AFTERNOON_START_TIME..(AFTERNOON_END_TIME - 1) ->
+            in AFTERNOON_START_TIME until AFTERNOON_END_TIME ->
                 R.string.main_records_new_day_title_afternoon
-            in EVENING_START_TIME..(EVENING_END_TIME - 1) ->
+            in EVENING_START_TIME until EVENING_END_TIME ->
                 R.string.main_records_new_day_title_evening
             else -> R.string.main_records_firs_launch_title
         }
