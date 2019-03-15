3b71205bf3d33eb1cbf3fde81a87810e69a37e02
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-03-16 00:31:40 +0200

    [CodeStyle] Fix code style issues

--- presentation/src/main/java/com/elta/android/presentation/features/main/events/base/initializer/InsulinFormInitializer.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/main/events/base/initializer/InsulinFormInitializer.kt
@@ -1,7 +1,6 @@
 package com.elta.android.presentation.features.main.events.base.initializer
 
 import android.content.res.Resources
-import android.support.v4.content.ContextCompat
 import android.view.View
 import com.elta.android.presentation.R
 import com.elta.android.presentation.widgets.picker.FormPicker
--- presentation/src/main/java/com/elta/android/presentation/features/main/events/base/initializer/MedicamentsFormInitializer.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/main/events/base/initializer/MedicamentsFormInitializer.kt
@@ -1,6 +1,5 @@
 package com.elta.android.presentation.features.main.events.base.initializer
 
-import android.support.v4.content.ContextCompat
 import android.view.View
 import com.elta.android.presentation.R
 import com.elta.android.presentation.widgets.picker.FormPicker
