9ddf51898b5b8167146e74a5cb4043a40281e56d
v.nykitchuk <v.nykitchuk@crysberry.com>
2019-03-18 13:36:35 +0200

    Subtask/elta 531/elta 539 profile screen ui

--- presentation/src/main/java/com/elta/android/presentation/features/profile/main/ui/MainProfileFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/profile/main/ui/MainProfileFragment.kt
@@ -5,6 +5,12 @@ import com.elta.android.presentation.core.ui.fragment.BaseListFragment
 import com.elta.android.presentation.core.ui.system_ui.LightStatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.features.profile.main.pm.MainProfilePm
+import com.elta.android.presentation.utils.appbar.AppBarState
+import com.elta.android.presentation.utils.appbar.collapseProgress
+import com.elta.android.presentation.utils.appbar.observeState
+import com.elta.android.presentation.utils.collapse
+import com.elta.android.presentation.utils.expand
+import kotlinx.android.synthetic.main.fragment_main_profile.*
 
 class MainProfileFragment : BaseListFragment<MainProfilePm>() {
 
@@ -16,6 +22,22 @@ class MainProfileFragment : BaseListFragment<MainProfilePm>() {
 
     override fun onBindPresentationModel(pm: MainProfilePm) {
         super.onBindPresentationModel(pm)
+        observeAppBarChanges()
+    }
+
+    private fun observeAppBarChanges() {
+        appBarLayoutView.observeState()
+            .bindTo {
+                toolbarUserNameTextView.apply {
+                    if (it == AppBarState.COLLAPSED) toolbarUserNameTextView.expand()
+                    else toolbarUserNameTextView.collapse()
+                }
+            }
+
+        appBarLayoutView.collapseProgress().bindTo {
+            val alpha = 1 - Math.abs(it / 100f)
+            profileInfoView.alpha = alpha
+        }
     }
 
     companion object {
