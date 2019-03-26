89b4834c1f624ada47702447f8d887f154b534ab
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-03-26 17:41:43 +0200

    [ELTA-651] Add DbSyncStorage to save sync info per user and per app

---
+++ data/src/main/java/com/elta/android/data/features/common/storage/DbSyncStorage.kt
@@ -0,0 +1,46 @@
+package com.elta.android.data.features.common.storage
+
+import com.elta.android.data.features.common.cache.BoxStoreFactory
+import io.objectbox.Box
+import io.objectbox.kotlin.boxFor
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DbSyncStorage @Inject constructor(
+    private val factory: BoxStoreFactory
+) : SyncStorage {
+
+    private val userBox: Box<SyncInfoDto>
+        get() = factory.getBoxStore().boxFor()
+
+    private val appBox: Box<SyncInfoDto>
+        get() = factory.getBoxStore(BoxStoreFactory.DbScope.PER_APP).boxFor()
+
+    private val userSyncInfo: SyncInfoDto?
+        get() = userBox.all.firstOrNull()
+
+    private val appSyncInfo: SyncInfoDto?
+        get() = appBox.all.firstOrNull()
+
+    override var lastSalePointsSync: Long?
+        get() = appSyncInfo?.lastSalePointsSync
+        set(value) {
+            appBox.put(appSyncInfo?.copy(lastSalePointsSync = value)
+                ?: SyncInfoDto(lastSalePointsSync = value))
+        }
+
+    override var lastEventsSync: Long?
+        get() = userSyncInfo?.lastEventsSync
+        set(value) {
+            userBox.put(userSyncInfo?.copy(lastEventsSync = value)
+                ?: SyncInfoDto(lastEventsSync = value))
+        }
+
+    override var lastTagsSync: Long?
+        get() = userSyncInfo?.lastTagsSync
+        set(value) {
+            userBox.put(userSyncInfo?.copy(lastTagsSync = value)
+                ?: SyncInfoDto(lastTagsSync = value))
+        }
+}
