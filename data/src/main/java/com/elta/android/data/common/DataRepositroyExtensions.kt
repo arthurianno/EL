dfc7089bbcdacc61080de579f816367261717110
Nikita G <grishko188@gmail.com>
2019-03-26 15:27:59 +0200

    [ELTA-638] Fix network crashes

---
+++ data/src/main/java/com/elta/android/data/common/DataRepositroyExtensions.kt
@@ -0,0 +1,60 @@
+package com.elta.android.data.common
+
+import com.elta.android.common.errors.NetworkConnectionError
+import com.elta.android.common.errors.ServerError
+import com.elta.android.common.errors.ServiceUnavailableError
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+
+fun <T> Observable<List<T>>.onConnectionErrorReturnsEmpty(): Observable<List<T>> =
+    this.onErrorReturn {
+        when (it.canIgnoreError()) {
+            true -> emptyList()
+            else -> throw it
+        }
+    }
+
+fun <T> Single<List<T>>.onConnectionErrorReturnsEmpty(): Single<List<T>> =
+    this.onErrorReturn {
+        when (it.canIgnoreError()) {
+            true -> emptyList()
+            else -> throw it
+        }
+    }
+
+fun <T> Observable<T>.onConnectionErrorResumeEmpty(): Observable<T> =
+    this.onErrorResumeNext { error: Throwable ->
+        when (error.canIgnoreError()) {
+            true -> Observable.empty<T>()
+            else -> Observable.error<T>(error)
+        }
+    }
+
+fun <T> Single<T>.onConnectionErrorReturnsDefault(factory: () -> T): Single<T> =
+    this.onErrorReturn {
+        when (it.canIgnoreError()) {
+            true -> factory.invoke()
+            else -> throw it
+        }
+    }
+
+fun <T> Single<T>.onConnectionErrorResumeDefault(factory: () -> Single<T>): Single<T> =
+    this.onErrorResumeNext {
+        when (it.canIgnoreError()) {
+            true -> factory.invoke()
+            else -> Single.error(it)
+        }
+    }
+
+fun Completable.onConnectionErrorCompletes(): Completable =
+    this.onErrorComplete { it.canIgnoreError() }
+
+private fun Throwable.canIgnoreError() =
+    this is NetworkConnectionError ||
+        this is ServerError ||
+        this is ServiceUnavailableError ||
+        this is ConnectException ||
+        this is SocketTimeoutException
