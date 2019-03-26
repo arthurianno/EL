a3fdca8c944d9a04a45f1b6b9bee18f18c63070d
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-03-26 17:41:58 +0200

    [ELTA-651] Add DbSyncStorage to save sync info per user and per app

--- data/src/main/java/com/elta/android/data/di/StorageModule.kt
+++ data/src/main/java/com/elta/android/data/di/StorageModule.kt
@@ -1,6 +1,6 @@
 package com.elta.android.data.di
 
-import com.elta.android.data.features.common.storage.LocalSyncStorage
+import com.elta.android.data.features.common.storage.DbSyncStorage
 import com.elta.android.data.features.common.storage.LocalUserHolder
 import com.elta.android.data.features.common.storage.SyncStorage
 import com.elta.android.data.features.common.storage.UserHolder
@@ -13,7 +13,7 @@ abstract class StorageModule {
 
     @Binds
     @Singleton
-    abstract fun bindSyncStorage(storage: LocalSyncStorage): SyncStorage
+    abstract fun bindSyncStorage(storage: DbSyncStorage): SyncStorage
 
     @Binds
     @Singleton
