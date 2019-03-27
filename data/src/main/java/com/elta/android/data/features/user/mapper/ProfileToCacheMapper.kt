9d5121358c26290624f68672227935225765872a
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-03-27 10:59:39 +0200

    [ELTA-651] Change profile id generation

--- data/src/main/java/com/elta/android/data/features/user/mapper/ProfileToCacheMapper.kt
+++ data/src/main/java/com/elta/android/data/features/user/mapper/ProfileToCacheMapper.kt
@@ -1,18 +1,15 @@
 package com.elta.android.data.features.user.mapper
 
 import com.elta.android.common.mapper.Mapper
-import com.elta.android.data.features.common.storage.UserHolder
 import com.elta.android.data.features.user.cache.dto.ProfileCacheDto
 import com.elta.android.data.features.user.dto.ProfileDto
 import javax.inject.Inject
 
-class ProfileToCacheMapper @Inject constructor(
-    private val userHolder: UserHolder
-) : Mapper<ProfileDto, ProfileCacheDto> {
+class ProfileToCacheMapper @Inject constructor() : Mapper<ProfileDto, ProfileCacheDto> {
     override fun mapFromObject(source: ProfileDto): ProfileCacheDto =
         with(source) {
             ProfileCacheDto(
-                id = userHolder.currentUser ?: 0,
+                id = email?.hashCode()?.toLong() ?: 0L,
                 diabetes = diabetes?.name,
                 weight = weight,
                 gender = gender?.name,
