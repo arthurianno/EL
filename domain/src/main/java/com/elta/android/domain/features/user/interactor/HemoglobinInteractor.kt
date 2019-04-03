336a510a00a21b5f5c661dd8f596a78e8008f3a1
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-04-03 10:39:27 +0300

    [ELTA-689] Add HemoglobinInteractor

---
+++ domain/src/main/java/com/elta/android/domain/features/user/interactor/HemoglobinInteractor.kt
@@ -0,0 +1,13 @@
+package com.elta.android.domain.features.user.interactor
+
+import com.elta.android.domain.features.user.model.Profile
+
+const val DEFAULT_VALUE = 5.0
+const val MIN = 2.0
+const val MAX = 12.0
+const val STEP = 0.1
+
+fun increment(original: Double): Double = if (original < MAX) original.plus(STEP) else MAX
+fun decrement(original: Double): Double = if (original > MIN) original.minus(STEP) else MIN
+
+fun Profile.getHemoglobinLevel(): Double = this.hba1cLevel ?: DEFAULT_VALUE
