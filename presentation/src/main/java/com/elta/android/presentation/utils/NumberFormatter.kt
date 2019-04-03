d5356ec1568d5ec24d1502877381e9b0cc7c6f4f
Volodymyr Chernyshov <v.chernyshov@nullgr.com>
2019-04-03 15:17:07 +0300

    [ELTA-687] Fix code style

--- presentation/src/main/java/com/elta/android/presentation/utils/NumberFormatter.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/NumberFormatter.kt
@@ -14,5 +14,5 @@ object NumberFormatter {
         }
     }
 
-    fun format(value: Double) : String = numberFormat.format(value)
+    fun format(value: Double): String = numberFormat.format(value)
 }
