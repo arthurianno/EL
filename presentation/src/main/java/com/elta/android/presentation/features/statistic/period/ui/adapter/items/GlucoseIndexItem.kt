0283da50f61b7b2acb49d4c3073315c4e738f194
v.chernyshov <v.chernyshov@nullgr.com>
2019-04-22 13:50:21 +0300

    [ELTA-795] Add GlucoseIndexItem

---
+++ presentation/src/main/java/com/elta/android/presentation/features/statistic/period/ui/adapter/items/GlucoseIndexItem.kt
@@ -0,0 +1,33 @@
+package com.elta.android.presentation.features.statistic.period.ui.adapter.items
+
+import android.graphics.drawable.Drawable
+import com.nullgr.core.adapter.items.ListItem
+
+data class GlucoseIndexItem(
+    val type: GlucoseIndexItem.Type,
+    val bg: Drawable?,
+    val value: String,
+    val unit: String,
+    val description: String
+) : ListItem {
+
+    override fun getUniqueProperty(): Any = type
+
+    override fun getChangePayload(other: ListItem): Any {
+        if (other is GlucoseIndexItem) {
+            return mutableSetOf<Payload>().apply {
+                if (type != other.type) add(Payload.TYPE_CHANGED)
+                if (value != other.value) add(Payload.VALUE_CHANGED)
+            }
+        }
+        return super.getChangePayload(other)
+    }
+
+    enum class Payload {
+        TYPE_CHANGED, VALUE_CHANGED
+    }
+
+    enum class Type {
+        AVERAGE, TOTAL, HIGH, NORMAL, LOW
+    }
+}
