0271d697bfaab6e6def951c081d47400cd283fa4
MacBook <macbook@MacBooks-MacBook-Pro.local>
2019-04-22 17:30:20 +0300

    [CodeStyle] Fix code style issues

--- presentation/src/main/java/com/elta/android/presentation/widgets/range_bar/RangeBarView.kt
+++ presentation/src/main/java/com/elta/android/presentation/widgets/range_bar/RangeBarView.kt
@@ -6,12 +6,12 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
+import android.support.annotation.ColorInt
+import android.support.annotation.ColorRes
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
-import androidx.annotation.ColorInt
-import androidx.annotation.ColorRes
 import com.elta.android.presentation.R
 import com.elta.android.presentation.utils.NumberFormatter
 import com.elta.android.presentation.utils.decodeBitmap
