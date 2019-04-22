539575fd07206c33e25241f34dfc20e7d40c6a04
v.chernyshov <v.chernyshov@nullgr.com>
2019-04-22 08:55:07 +0300

    [ELTA-792] Complete BuildStatisticModelTest

--- domain/src/test/java/com/elta/android/domain/statistic/BuildStatisticModelTest.kt
+++ domain/src/test/java/com/elta/android/domain/statistic/BuildStatisticModelTest.kt
@@ -6,21 +6,28 @@ import com.elta.android.domain.features.diary.events.model.EventType
 import com.elta.android.domain.features.diary.events.model.InsulinType
 import com.elta.android.domain.features.diary.home.model.GlucoseLevelSettings
 import com.elta.android.domain.features.statistics.interactor.buildStatisticModel
+import com.elta.android.domain.features.statistics.interactor.percent
 import com.elta.android.domain.features.statistics.model.ActivityStatisticModel
 import com.elta.android.domain.features.statistics.model.BreadStatisticModelByPeriod
 import com.elta.android.domain.features.statistics.model.GlucoseStatisticModel
 import com.elta.android.domain.features.statistics.model.InsulinStatisticModelByPeriod
 import com.elta.android.domain.features.statistics.model.Periods
 import com.elta.android.domain.features.statistics.model.StatisticByPeriodModel
+import com.elta.android.domain.features.statistics.model.daily.DailyBreadStatisticModel
+import com.elta.android.domain.features.statistics.model.daily.DailyInsulinStatisticModel
 import com.elta.android.domain.features.statistics.model.daily.DailyStatisticModel
+import com.elta.android.domain.features.user.interactor.round
+import com.nullgr.core.date.withoutTime
+import org.junit.Test
 import java.util.Date
 
 class BuildStatisticModelTest {
 
+    @Test
     fun buildStatisticModel_correct() {
         val period = Periods.SevenDays()
 
-        val day = Date()
+        val day = Date().withoutTime()
 
         val glucoseEvents = arrayListOf(
             EventTestFactory.create(type = EventType.GLUCOSE, value = 100.0),
@@ -67,10 +74,10 @@ class BuildStatisticModelTest {
 
         val expected = StatisticByPeriodModel(
             period = period,
-            dayWithMaxLevel = getExpectedDayWithMaxLevel(),
-            dayWithMinLevel = getExpectedDayWithMinLevel(),
-            allDays = getExpectedAllDays(),
-            glucose = getExpectedGlucoseStatistic(),
+            dayWithMaxLevel = getExpectedDayWithMaxLevel(day, glucoseEvents),
+            dayWithMinLevel = getExpectedDayWithMinLevel(day, glucoseEvents),
+            allDays = getExpectedAllDays(day, glucoseEvents),
+            glucose = getExpectedGlucoseStatistic(glucoseEvents),
             insulin = getExpectedInsulinStatistic(),
             bread = getExpectedBreadStatistic(),
             activity = getExpectedActivityStatistic()
@@ -81,31 +88,101 @@ class BuildStatisticModelTest {
         assert(model == expected)
     }
 
-    private fun getExpectedDayWithMaxLevel() : DailyStatisticModel {
-
+    private fun getExpectedDayWithMaxLevel(day: Date, glucoseEvents: List<Event>): DailyStatisticModel {
+        return getExpectedDailyStatisticModel(day, glucoseEvents)
     }
 
-    private fun getExpectedDayWithMinLevel() : DailyStatisticModel {
-
+    private fun getExpectedDayWithMinLevel(day: Date, glucoseEvents: List<Event>): DailyStatisticModel {
+        return getExpectedDailyStatisticModel(day, glucoseEvents)
     }
 
-    private fun getExpectedAllDays(): Map<Date, DailyStatisticModel> {
-
+    private fun getExpectedAllDays(day: Date, glucoseEvents: List<Event>): Map<Date, DailyStatisticModel> {
+        val dailyStatisticModel = getExpectedDailyStatisticModel(day, glucoseEvents)
+        return mapOf(day to dailyStatisticModel)
     }
 
-    private fun getExpectedGlucoseStatistic(): GlucoseStatisticModel {
+    private fun getExpectedDailyStatisticModel(day: Date, glucoseEvents: List<Event>): DailyStatisticModel {
+        return DailyStatisticModel(
+            date = day,
+            glucose = GlucoseStatisticModel(
+                averageLevel = (glucoseEvents.sumByDouble {
+                    it.value ?: 0.0
+                } / glucoseEvents.size).round(2),
+                maxLevel = 100.0,
+                minLevel = 2.0,
 
+                maxHighLevel = 100.0,
+                minHighLevel = 20.0,
+
+                maxNormalLevel = 10.0,
+                minNormalLevel = 5.0,
+
+                maxLowLevel = 2.0,
+                minLowLevel = 2.0,
+
+                eventsCount = glucoseEvents.size,
+                eventsHighCount = 2,
+                eventsNormalCount = 2,
+                eventsLowCount = 1,
+
+                eventsHighPercent = 2.percent(glucoseEvents.size),
+                eventsNormalPercent = 2.percent(glucoseEvents.size),
+                eventsLowPercent = 1.percent(glucoseEvents.size)
+            ),
+            insulin = DailyInsulinStatisticModel(
+                totalBolusLevel = 20.0,
+                totalBasalLevel = 30.0,
+                totalLevel = 50.0
+            ),
+            bread = DailyBreadStatisticModel(
+                totalLevel = 20.0
+            ),
+            activity = ActivityStatisticModel(
+                eventsCount = 4,
+                averageDuration = 75L
+            )
+        )
+    }
+
+    private fun getExpectedGlucoseStatistic(events: List<Event>): GlucoseStatisticModel {
+        return GlucoseStatisticModel(
+            averageLevel = (events.sumByDouble { it.value ?: 0.0 } / events.size).round(2),
+            maxLevel = 100.0,
+            minLevel = 2.0,
+
+            maxHighLevel = 100.0,
+            minHighLevel = 20.0,
+
+            maxNormalLevel = 10.0,
+            minNormalLevel = 5.0,
+
+            maxLowLevel = 2.0,
+            minLowLevel = 2.0,
+
+            eventsCount = events.size,
+            eventsHighCount = 2,
+            eventsNormalCount = 2,
+            eventsLowCount = 1,
+
+            eventsHighPercent = 2.percent(events.size),
+            eventsNormalPercent = 2.percent(events.size),
+            eventsLowPercent = 1.percent(events.size)
+        )
     }
 
     private fun getExpectedInsulinStatistic(): InsulinStatisticModelByPeriod {
-
+        return InsulinStatisticModelByPeriod(
+            averageBolusLevel = 10.0,
+            averageBasalLevel = 10.0,
+            averageLevel = 10.0
+        )
     }
 
     private fun getExpectedBreadStatistic(): BreadStatisticModelByPeriod {
-
+        return BreadStatisticModelByPeriod(averageLevel = 10.0)
     }
 
     private fun getExpectedActivityStatistic(): ActivityStatisticModel {
-
+        return ActivityStatisticModel(eventsCount = 4, averageDuration = 75L)
     }
 }
