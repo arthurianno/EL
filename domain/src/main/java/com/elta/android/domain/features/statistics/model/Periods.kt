e9eaa24077f71e63a7d41999a81bb8967d3557fe
v.chernyshov <v.chernyshov@nullgr.com>
2019-04-23 14:39:30 +0300

    [ELTA-799] Fix period calculation

--- domain/src/main/java/com/elta/android/domain/features/statistics/model/Periods.kt
+++ domain/src/main/java/com/elta/android/domain/features/statistics/model/Periods.kt
@@ -10,22 +10,22 @@ import java.util.Date
 sealed class Periods(override val start: Date, override val end: Date) : StatisticPeriod {
 
     class SevenDays : Periods(
-        start = Date().minusDay(7).withoutTime(),
+        start = Date().minusDay(6).withoutTime(),
         end = Date().atEndOfDay()
     )
 
     class FourteenDays : Periods(
-        start = Date().minusDay(14).withoutTime(),
+        start = Date().minusDay(13).withoutTime(),
         end = Date().atEndOfDay()
     )
 
     class ThirtyDays : Periods(
-        start = Date().minusDay(30).withoutTime(),
+        start = Date().minusDay(29).withoutTime(),
         end = Date().atEndOfDay()
     )
 
     class NinetyDays : Periods(
-        start = Date().minusDay(90).withoutTime(),
+        start = Date().minusDay(89).withoutTime(),
         end = Date().atEndOfDay()
     )
 }
