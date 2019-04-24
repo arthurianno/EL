b7c2cc7f52f1221c214543001e17116ea9c6153e
Yuriy Getsko <y.getsko@crysberry.com>
2019-04-24 19:23:44 +0300

    [ELTA-939] Fix past time in time picker for reminders

--- presentation/src/main/java/com/elta/android/presentation/utils/DatePickerExtensions.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/DatePickerExtensions.kt
@@ -3,6 +3,9 @@ package com.elta.android.presentation.utils
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.Context
+import android.widget.Toast
+import com.elta.android.presentation.R
+import java.util.Calendar
 import java.util.Date
 
 fun Context?.showDatePickerDialog(
@@ -24,15 +27,39 @@ fun Context?.showDatePickerDialog(
     }
 }
 
-fun Context?.showTimePickerDialog(
-    currentDate: Date,
-    onDateSelectedFunction: (Date) -> Unit
-) {
+@Suppress("MagicNumber")
+fun Context?.showTimePickerDialog(selectedDate: Date, onDateSelectedFunction: (Date) -> Unit) {
     if (this == null) return
-    val c = currentDate.toCalendar()
-    val onTimeSelectedListener = TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
-        c.set(c.year, c.month, c.dayOfMonth, hourOfDay, minute)
-        onDateSelectedFunction.invoke(c.time)
-    }
-    TimePickerDialog(this, onTimeSelectedListener, c.hourOfDay, c.minute, true).show()
+    val c = selectedDate.toCalendar()
+    val onTimeSelectedListener =
+        TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+            val currentCalendar = Calendar.getInstance().apply {
+                // It needs to prevent choose current minute on time picker.
+                add(Calendar.MINUTE, 1)
+            }
+            val pickedCalendar = Calendar.getInstance().apply {
+                set(Calendar.HOUR_OF_DAY, hourOfDay)
+                set(Calendar.MINUTE, minute)
+            }
+            if (pickedCalendar.before(currentCalendar)) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.profile_reminders_wrong_time),
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                c.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                c.set(Calendar.MINUTE, minute)
+                onDateSelectedFunction.invoke(c.time)
+            }
+        }
+
+    TimePickerDialog(
+        this,
+        onTimeSelectedListener,
+        c.hourOfDay,
+        c.minute,
+        true
+    )
+        .show()
 }
