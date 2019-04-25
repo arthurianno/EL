62f5f36dc03daa13c2f6362fad59e2328e0bd04a
v.chernyshov <v.chernyshov@nullgr.com>
2019-04-25 20:10:47 +0300

    [ELTA-947] Add part of logic at ConnectDevicePm

--- presentation/src/main/java/com/elta/android/presentation/features/sync/connect/pm/ConnectDevicePm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/sync/connect/pm/ConnectDevicePm.kt
@@ -1,13 +1,189 @@
 package com.elta.android.presentation.features.sync.connect.pm
 
+import com.elta.android.common.errors.BluetoothNotEnabledError
+import com.elta.android.common.errors.GlucometerPinIncorrectOrNotFoundError
+import com.elta.android.common.errors.LocationNotEnabledError
+import com.elta.android.common.errors.LocationPermissionNotGrantedError
+import com.elta.android.domain.features.devices.interactor.FindGlucometersUseCase
+import com.elta.android.domain.features.devices.interactor.SetPinCodeUseCase
+import com.elta.android.domain.features.devices.model.Glucometer
+import com.elta.android.presentation.Clicks
+import com.elta.android.presentation.Events
+import com.elta.android.presentation.R
+import com.elta.android.presentation.Screens
+import com.elta.android.presentation.core.bus.clicks
+import com.elta.android.presentation.core.bus.events
 import com.elta.android.presentation.core.pm.BaseListPm
 import com.elta.android.presentation.core.pm.ServiceFacade
+import com.elta.android.presentation.core.pm.widgets.snackBarControl
+import com.elta.android.presentation.core.ui.snack_bar_view.SnackBarData
+import com.elta.android.presentation.features.sync.connect.ui.adapter.items.DeviceItem
+import com.elta.android.presentation.messages.SnackBarMessageData
+import io.reactivex.Observable
+import java.util.concurrent.TimeoutException
 import javax.inject.Inject
 
 class ConnectDevicePm @Inject constructor(
+    private val setPinCodeUseCase: SetPinCodeUseCase,
+    private val findGlucometersUseCase: FindGlucometersUseCase,
     services: ServiceFacade
 ) : BaseListPm(services) {
 
     val mainAction = Action<Unit>()
     val skipAction = Action<Unit>()
+    val connectDeviceAction = Action<Unit>()
+
+    val sendPinCodeToDeviceAction = Action<String>()
+
+    val openPinCodeDialogCommand = Command<String>(bufferSize = 1)
+
+    val state = State(ViewState.SEARCH)
+
+    val requestEnableBluetoothCommand = Command<Unit>(bufferSize = 1)
+    val requestLocationPermissionsCommand = Command<Unit>(bufferSize = 1)
+    val requestEnableLocationCommand = Command<Unit>(bufferSize = 1)
+
+    val bluetoothEnabledAction = Action<Unit>()
+    val locationPermissionsGrantedAction = Action<Unit>()
+    val locationEnabledAction = Action<Unit>()
+
+    private val scanResults = mutableSetOf<Glucometer>()
+    private var glucometer: Glucometer? = null
+
+    private val startScanAction = Action<Unit>()
+
+    private val deviceNotFound: SnackBarData by lazy {
+        SnackBarMessageData.WithButton(
+            resources.getString(R.string.sync_connect_device_not_found),
+            resources.getString(R.string.sync_connect_button_retry)
+        )
+    }
+
+    private val incorrectPinCode: SnackBarData by lazy {
+        SnackBarMessageData.WithButton(
+            resources.getString(R.string.sync_connect_incorrect_pin_code),
+            resources.getString(R.string.sync_connect_button_retry)
+        )
+    }
+
+    private val showRetrySearchAction = Action<Unit>()
+    private val showRetryPinAction = Action<Unit>()
+
+    val retrySearchControl = snackBarControl<SnackBarData>()
+    val retryPinControl = snackBarControl<SnackBarData>()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startScanAction.observable
+            .flatMap {
+                findGlucometersUseCase.execute()
+                    .doOnNext { results ->
+                        if (results.isNotEmpty()) {
+                            state.consumer.accept(ViewState.FOUND)
+                            retrySearchControl.dismiss()
+                        }
+                        scanResults.clear()
+                        scanResults.addAll(results)
+                        items.consumer.accept(
+                            results.map { meter ->
+                                DeviceItem(
+                                    id = meter.id,
+                                    name = meter.name ?: "Unknown device",
+                                    address = meter.address,
+                                    isSelected = meter.address == glucometer?.address
+                                )
+                            }
+                        )
+                    }
+                    .doOnError(::handleError)
+            }
+            .retry()
+            .subscribe()
+            .untilDestroy()
+
+        showRetrySearchAction.observable
+            .switchMapMaybe {
+                retrySearchControl.showForResult(deviceNotFound)
+            }
+            .subscribe(startScanAction.consumer)
+            .untilDestroy()
+
+        showRetryPinAction.observable
+            .switchMapMaybe {
+                retryPinControl.showForResult(incorrectPinCode)
+            }
+            .subscribe(connectDeviceAction.consumer)
+            .untilDestroy()
+
+        skipAction.observable
+            .doOnNext(::navigateToShopsFlow)
+            .subscribe()
+            .untilDestroy()
+
+        connectDeviceAction.observable
+            .map { glucometer?.name ?: "SatelliteOnline" }
+            .subscribe(openPinCodeDialogCommand.consumer)
+            .untilDestroy()
+
+        bus.events<Events.PinCodeEntered>()
+            .skipWhileInProgress()
+            .map(Events.PinCodeEntered::pin)
+            .map { pin ->
+                SetPinCodeUseCase.Params(glucometer?.address ?: "", pin)
+            }
+            .flatMapCompletable { params ->
+                setPinCodeUseCase.execute(params)
+                    .bindProgress()
+                    .doOnComplete {
+                        state.consumer.accept(ViewState.CONNECTED)
+                    }
+                    .doOnError(::handleError)
+            }
+            .retry()
+            .subscribe()
+            .untilDestroy()
+
+        bus.clicks<Clicks.DeviceClicked>()
+            .doOnNext { click ->
+                glucometer = scanResults.firstOrNull { it.address == click.item.address }
+                val newItems = items.value.map { (it as DeviceItem).copy(isSelected = it.address == click.item.address && !it.isSelected) }
+                items.consumer.accept(newItems)
+            }
+            .subscribe()
+            .untilDestroy()
+
+        Observable.merge(
+            lifecycleObservable.filter { it == Lifecycle.CREATED }.map { Unit },
+            bluetoothEnabledAction.observable,
+            locationPermissionsGrantedAction.observable,
+            locationEnabledAction.observable
+        )
+            .subscribe(startScanAction.consumer)
+            .untilDestroy()
+    }
+
+    override fun handleError(error: Throwable) {
+        when (error) {
+            is BluetoothNotEnabledError -> requestEnableBluetoothCommand.consumer.accept(Unit)
+            is LocationPermissionNotGrantedError -> requestLocationPermissionsCommand.consumer.accept(Unit)
+            is LocationNotEnabledError -> requestEnableLocationCommand.consumer.accept(Unit)
+            is TimeoutException -> {
+                val devices = items.valueOrNull
+                if (devices == null || devices.isEmpty()) {
+                    showRetrySearchAction.consumer.accept(Unit)
+                }
+            }
+            is GlucometerPinIncorrectOrNotFoundError -> showRetryPinAction.consumer.accept(Unit)
+            else -> super.handleError(error)
+        }
+    }
+
+    private fun navigateToShopsFlow(i: Unit) {
+        router.newRootFlow(Screens.ShopsFlow)
+    }
+
+    enum class ViewState {
+        SEARCH, FOUND, CONNECTED,
+    }
 }
--- presentation/src/main/java/com/elta/android/presentation/features/sync/connect/ui/ConnectDeviceFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/sync/connect/ui/ConnectDeviceFragment.kt
@@ -1,22 +1,45 @@
 package com.elta.android.presentation.features.sync.connect.ui
 
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v4.content.ContextCompat
 import android.view.View
 import com.elta.android.presentation.R
-import com.elta.android.presentation.core.ui.fragment.BaseFragment
+import com.elta.android.presentation.core.ui.fragment.BaseListFragment
 import com.elta.android.presentation.core.ui.system_ui.LightStatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.features.sync.connect.pm.ConnectDevicePm
+import com.elta.android.presentation.features.sync.pin.ui.PinDialogFragment
+import com.elta.android.presentation.utils.makeSnackBar
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.location.LocationSettingsStatusCodes
+import com.google.android.gms.location.SettingsClient
 import com.jakewharton.rxbinding2.view.clicks
+import com.nullgr.core.intents.launchForResult
+import com.nullgr.core.ui.extensions.children
 import com.nullgr.core.ui.extensions.hide
+import com.nullgr.core.ui.extensions.toggleView
+import com.nullgr.core.ui.fragments.showDialog
+import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.fragment_sync_connect.*
+import kotlinx.android.synthetic.main.layout_sync_state_device_found.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
+import timber.log.Timber
 
-class ConnectDeviceFragment : BaseFragment<ConnectDevicePm>() {
+class ConnectDeviceFragment : BaseListFragment<ConnectDevicePm>() {
 
     override val screenLayout: Int = R.layout.fragment_sync_connect
     override val classToken: Class<ConnectDevicePm> = ConnectDevicePm::class.java
     override val statusBarConfigProvider: StatusBarConfigProvider = LightStatusBarConfigProvider
+
+    private val rxPermissions by lazy { RxPermissions(this) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,9 +50,93 @@ class ConnectDeviceFragment : BaseFragment<ConnectDevicePm>() {
     override fun onBindPresentationModel(pm: ConnectDevicePm) {
         super.onBindPresentationModel(pm)
         menuButtonView.clicks().bindTo(pm.skipAction)
+        actionButtonView.clicks().bindTo(pm.connectDeviceAction)
+        pm.state.bindTo { state ->
+            syncStateContainerView.children().forEach { view ->
+                view.toggleView(state.getId() == view.id)
+            }
+        }
+
+        pm.retrySearchControl.bindTo { data, sc ->
+            makeSnackBar(view!!, data)
+                .setDuration(Snackbar.LENGTH_INDEFINITE)
+                .setActionTextColor(ContextCompat.getColor(context!!, R.color.shade_blue))
+                .setAction(data.button) { sc.sendResult() }
+        }
+
+        pm.retryPinControl.bindTo { data, sc ->
+            makeSnackBar(view!!, data)
+                .setDuration(Snackbar.LENGTH_INDEFINITE)
+                .setActionTextColor(ContextCompat.getColor(context!!, R.color.shade_blue))
+                .setAction(data.button) { sc.sendResult() }
+        }
+
+        pm.requestEnableBluetoothCommand.observable
+            .bindTo {
+                Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                    .launchForResult(checkNotNull(activity), REQUEST_CODE_ENABLE_BLUETOOTH)
+            }
+        pm.requestLocationPermissionsCommand.observable
+            .switchMap {
+                rxPermissions.request(android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                    .filter { it }
+                    .map { Unit }
+            }
+            .bindTo(pm.locationPermissionsGrantedAction)
+        pm.requestEnableLocationCommand.observable
+            .bindTo {
+                val result = SettingsClient(checkNotNull(context))
+                    .checkLocationSettings(
+                        LocationSettingsRequest.Builder()
+                            .addLocationRequest(LocationRequest.create())
+                            .setNeedBle(true)
+                            .build()
+                    )
+                result.addOnCompleteListener { task ->
+                    try {
+                        task.getResult(ApiException::class.java)
+                    } catch (e: ApiException) {
+                        when (e.statusCode) {
+                            LocationSettingsStatusCodes.RESOLUTION_REQUIRED ->
+                                try {
+                                    (e as? ResolvableApiException)?.startResolutionForResult(
+                                        checkNotNull(activity),
+                                        REQUEST_CODE_ENABLE_LOCATION
+                                    )
+                                } catch (e1: IntentSender.SendIntentException) {
+                                    Timber.e(e1)
+                                }
+                        }
+                    }
+                }
+            }
+        pm.openPinCodeDialogCommand.bindTo {
+            childFragmentManager.showDialog(PinDialogFragment.newInstance(it))
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_ENABLE_LOCATION && resultCode == Activity.RESULT_OK) {
+            presentationModel.locationEnabledAction.consumer.accept(Unit)
+        }
+
+        if (requestCode == REQUEST_CODE_ENABLE_BLUETOOTH && resultCode == Activity.RESULT_OK) {
+            presentationModel.bluetoothEnabledAction.consumer.accept(Unit)
+        }
+    }
+
+    private inline fun ConnectDevicePm.ViewState.getId() =
+        when (this) {
+            ConnectDevicePm.ViewState.SEARCH -> R.id.stateConnectView
+            ConnectDevicePm.ViewState.FOUND -> R.id.stateDeviceFoundView
+            ConnectDevicePm.ViewState.CONNECTED -> R.id.stateConnectedView
+        }
+
     companion object {
+        private const val REQUEST_CODE_ENABLE_LOCATION = 145
+        private const val REQUEST_CODE_ENABLE_BLUETOOTH = 146
+
         fun newInstance(): ConnectDeviceFragment = ConnectDeviceFragment()
     }
 }
