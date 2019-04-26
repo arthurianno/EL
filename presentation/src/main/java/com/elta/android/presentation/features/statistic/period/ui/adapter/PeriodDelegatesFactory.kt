2d6c8746a8d189d70cefcb31a3d6f1bddc016905
Nikita G <grishko188@gmail.com>
2019-04-26 16:49:10 +0300

    [ELTA-944] Glucose statistics chart item

--- presentation/src/main/java/com/elta/android/presentation/features/statistic/period/ui/adapter/PeriodDelegatesFactory.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/statistic/period/ui/adapter/PeriodDelegatesFactory.kt
@@ -6,22 +6,27 @@ import com.elta.android.presentation.features.profile.settings.global.ui.adapter
 import com.elta.android.presentation.features.statistic.period.ui.adapter.delegates.GeneralIndexDelegate
 import com.elta.android.presentation.features.statistic.period.ui.adapter.delegates.GlucoseIndexDelegate
 import com.elta.android.presentation.features.statistic.period.ui.adapter.delegates.GlucoseIndexesDelegate
+import com.elta.android.presentation.features.statistic.period.ui.adapter.delegates.GlucoseStatisticChartDelegate
 import com.elta.android.presentation.features.statistic.period.ui.adapter.items.GeneralIndexItem
 import com.elta.android.presentation.features.statistic.period.ui.adapter.items.GlucoseIndexItem
 import com.elta.android.presentation.features.statistic.period.ui.adapter.items.GlucoseIndexesItem
+import com.elta.android.presentation.features.statistic.period.ui.adapter.items.GlucoseStatisticChartItem
 import com.nullgr.core.adapter.AdapterDelegate
 import com.nullgr.core.adapter.AdapterDelegatesFactory
 import com.nullgr.core.adapter.DiffCalculator
 import com.nullgr.core.adapter.items.ListItem
+import com.nullgr.core.rx.RxBus
 import javax.inject.Inject
 
 class PeriodDelegatesFactory @Inject constructor(
+    private val bus: RxBus,
     private val viewPool: RecyclerView.RecycledViewPool,
     private val calculator: DiffCalculator
 ) : AdapterDelegatesFactory {
 
     override fun createDelegate(clazz: Class<ListItem>): AdapterDelegate =
         when (clazz) {
+            GlucoseStatisticChartItem::class.java -> GlucoseStatisticChartDelegate(bus)
             GlucoseIndexItem::class.java -> GlucoseIndexDelegate()
             GlucoseIndexesItem::class.java -> GlucoseIndexesDelegate(this, calculator, viewPool)
             GeneralIndexItem::class.java -> GeneralIndexDelegate()
---
+++ presentation/src/main/java/com/elta/android/presentation/features/statistic/period/ui/adapter/delegates/GlucoseStatisticChartDelegate.kt
@@ -0,0 +1,23 @@
+package com.elta.android.presentation.features.statistic.period.ui.adapter.delegates
+
+import android.support.v7.widget.RecyclerView
+import com.elta.android.presentation.R
+import com.elta.android.presentation.features.statistic.period.ui.adapter.items.GlucoseStatisticChartItem
+import com.nullgr.core.adapter.items.ListItem
+import com.nullgr.core.adapter.ktx.AdapterDelegate
+import com.nullgr.core.adapter.ktx.ViewHolder
+import com.nullgr.core.rx.RxBus
+import kotlinx.android.synthetic.main.item_glucose_statistic_chart.*
+
+class GlucoseStatisticChartDelegate(val bus: RxBus) : AdapterDelegate() {
+
+    override val layoutResource: Int = R.layout.item_glucose_statistic_chart
+    override val itemType: Any = GlucoseStatisticChartItem::class
+
+    override fun onBindViewHolder(items: List<ListItem>, position: Int, holder: RecyclerView.ViewHolder) {
+        val item = items[position] as GlucoseStatisticChartItem
+        with(holder as ViewHolder) {
+            periodDatesTitleView.text = item.datesTitle
+        }
+    }
+}
