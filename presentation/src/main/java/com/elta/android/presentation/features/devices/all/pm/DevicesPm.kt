ff1b9d735a600aa3b1038b32c08885cec318fbdd
Dmitriy <dmitriy.makovetskiyua@gmail.com>
2019-05-14 17:46:24 +0300

    [ELTA-1113] Header devices screen item

--- presentation/src/main/java/com/elta/android/presentation/features/devices/all/pm/DevicesPm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/devices/all/pm/DevicesPm.kt
@@ -2,8 +2,10 @@ package com.elta.android.presentation.features.devices.all.pm
 
 import com.elta.android.presentation.core.pm.BaseListPm
 import com.elta.android.presentation.core.pm.ServiceFacade
+import com.elta.android.presentation.features.devices.all.ui.builder.DevicesOptionsItemsBuilder
 import javax.inject.Inject
 
 class DevicesPm @Inject constructor(
+    private val itemsBuilder: DevicesOptionsItemsBuilder,
     services: ServiceFacade
 ) : BaseListPm(services)
