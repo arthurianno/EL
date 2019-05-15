35caab2b1ec5d3ba87adeb0695ff01d78af59a78
v.nykitchuk <v.nykitchuk@crysberry.com>
2019-05-15 10:07:11 +0300

    [ELTA-1118] Add delete dialog

--- presentation/src/main/java/com/elta/android/presentation/features/devices/info/pm/DeviceInfoPm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/devices/info/pm/DeviceInfoPm.kt
@@ -1,16 +1,43 @@
 package com.elta.android.presentation.features.devices.info.pm
 
+import com.elta.android.presentation.Dialogs
 import com.elta.android.presentation.core.pm.BaseListPm
 import com.elta.android.presentation.core.pm.ServiceFacade
+import com.elta.android.presentation.core.ui.dialog.DialogData
 import com.elta.android.presentation.features.devices.info.ui.adapter.items.DeviceInfoItem
+import me.dmdev.rxpm.widget.dialogControl
 import javax.inject.Inject
 
 class DeviceInfoPm @Inject constructor(
     services: ServiceFacade
 ) : BaseListPm(services) {
 
+    val deleteDeviceDialogControl = dialogControl<DialogData, DialogResult>()
+
+     val deleteDeviceAction = Action<Unit>()
+
+    private val deleteDeviceDialogData: DialogData by lazy { Dialogs.DeleteDevice(resources) }
+
+
     override fun onCreate() {
         super.onCreate()
+
+        deleteDeviceAction.observable
+            .skipWhileInProgress()
+            .switchMapMaybe {
+                deleteDeviceDialogControl.showForResult(deleteDeviceDialogData)
+            }
+            .filter { it == DialogResult.POSITIVE }
+//            .flatMapCompletable { params ->
+//                deleteObserverUseCase.execute(params)
+//                    .hideErrorContainer()
+//                    .bindProgress()
+//                    .doOnComplete(::handleDeletingSuccess)
+//                    .doOnError(::handleError)
+//            }
+            .retry()
+            .subscribe()
+            .untilDestroy()
 
         // todo only for testing
         lifecycleObservable.filter { it == Lifecycle.CREATED }
@@ -18,5 +45,9 @@ class DeviceInfoPm @Inject constructor(
             .doOnNext(items.consumer)
             .subscribe()
             .untilDestroy()
+    }
+
+    enum class DialogResult {
+        NEGATIVE, POSITIVE
     }
 }
--- presentation/src/main/java/com/elta/android/presentation/features/devices/info/ui/DeviceInfoFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/devices/info/ui/DeviceInfoFragment.kt
@@ -2,11 +2,13 @@ package com.elta.android.presentation.features.devices.info.ui
 
 import android.os.Bundle
 import android.view.View
+import com.afollestad.materialdialogs.MaterialDialog
 import com.elta.android.presentation.R
 import com.elta.android.presentation.core.ui.fragment.BaseListFragment
 import com.elta.android.presentation.core.ui.system_ui.LightStatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.features.devices.info.pm.DeviceInfoPm
+import com.jakewharton.rxbinding2.view.clicks
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
 class DeviceInfoFragment : BaseListFragment<DeviceInfoPm>() {
@@ -23,6 +25,19 @@ class DeviceInfoFragment : BaseListFragment<DeviceInfoPm>() {
     override fun onBindPresentationModel(pm: DeviceInfoPm) {
         super.onBindPresentationModel(pm)
         bindProgressDialog(pm)
+        menuButtonView.clicks().bindTo(pm.deleteDeviceAction)
+
+        pm.deleteDeviceDialogControl.bindTo { data, dc ->
+            MaterialDialog.Builder(checkNotNull(activity))
+                .cancelable(false)
+                .title(data.title)
+                .content(data.message)
+                .negativeText(data.negative)
+                .positiveText(data.positive)
+                .onPositive { _, _ -> dc.sendResult(DeviceInfoPm.DialogResult.POSITIVE) }
+                .onNegative { _, _ -> dc.sendResult(DeviceInfoPm.DialogResult.NEGATIVE) }
+                .build()
+        }
     }
 
     companion object {
