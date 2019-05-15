5c97598039ea152a29cc971ba90795f7db9612f2
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-15 13:32:01 +0300

    [ELTA-947] Bind mapper from GlucometerEventDto to Event

--- data/src/main/java/com/elta/android/data/di/MappersModule.kt
+++ data/src/main/java/com/elta/android/data/di/MappersModule.kt
@@ -5,6 +5,7 @@ import com.elta.android.data.features.auth.dto.SocialUserDto
 import com.elta.android.data.features.auth.mapper.SocialUserDtoMapper
 import com.elta.android.data.features.devices.cache.dto.GlucometerCachedDto
 import com.elta.android.data.features.devices.dto.GlucometerDto
+import com.elta.android.data.features.devices.dto.GlucometerEventDto
 import com.elta.android.data.features.devices.dto.GlucometerInfoDto
 import com.elta.android.data.features.devices.mapper.GlucometerInfoToDomainMapper
 import com.elta.android.data.features.devices.mapper.GlucometerToCacheMapper
@@ -15,6 +16,7 @@ import com.elta.android.data.features.diary.events.cache.dto.EventCachedDto
 import com.elta.android.data.features.diary.events.dto.EventDto
 import com.elta.android.data.features.diary.events.dto.SimpleEventDto
 import com.elta.android.data.features.diary.events.mapper.EventFromCacheMapper
+import com.elta.android.data.features.diary.events.mapper.EventFromGlucometerMapper
 import com.elta.android.data.features.diary.events.mapper.EventToCacheMapper
 import com.elta.android.data.features.diary.events.mapper.EventToDomainMapper
 import com.elta.android.data.features.diary.events.mapper.EventToDtoMapper
@@ -28,6 +30,11 @@ import com.elta.android.data.features.firmware.dto.FirmwareDto
 import com.elta.android.data.features.firmware.dto.FirmwareFileDto
 import com.elta.android.data.features.firmware.mapper.FirmwareFileToDomainMapper
 import com.elta.android.data.features.firmware.mapper.FirmwareToDomainMapper
+import com.elta.android.data.features.observers.cache.dto.ObserverCacheDto
+import com.elta.android.data.features.observers.dto.ObserverDto
+import com.elta.android.data.features.observers.mapper.ObserverFromCacheMapper
+import com.elta.android.data.features.observers.mapper.ObserverToCacheMapper
+import com.elta.android.data.features.observers.mapper.ObserverToDomainMapper
 import com.elta.android.data.features.reminder.cache.dto.ReminderCacheDto
 import com.elta.android.data.features.reminder.dto.ReminderDto
 import com.elta.android.data.features.reminder.mapper.ReminderFromCacheMapper
@@ -42,17 +49,12 @@ import com.elta.android.data.features.sale_points.mapper.SalePointFromCacheMappe
 import com.elta.android.data.features.sale_points.mapper.SalePointToCacheMapper
 import com.elta.android.data.features.sale_points.mapper.SalePointToDomainMapper
 import com.elta.android.data.features.user.cache.dto.NetworkCacheDto
-import com.elta.android.data.features.observers.cache.dto.ObserverCacheDto
 import com.elta.android.data.features.user.cache.dto.ProfileCacheDto
-import com.elta.android.data.features.observers.dto.ObserverDto
 import com.elta.android.data.features.user.dto.ProfileDto
 import com.elta.android.data.features.user.dto.SocialNetworkDto
 import com.elta.android.data.features.user.mapper.NetworkFromCacheMapper
 import com.elta.android.data.features.user.mapper.NetworkToCacheMapper
 import com.elta.android.data.features.user.mapper.NetworkToDomainMapper
-import com.elta.android.data.features.observers.mapper.ObserverFromCacheMapper
-import com.elta.android.data.features.observers.mapper.ObserverToCacheMapper
-import com.elta.android.data.features.observers.mapper.ObserverToDomainMapper
 import com.elta.android.data.features.user.mapper.ProfileFromCacheMapper
 import com.elta.android.data.features.user.mapper.ProfileToCacheMapper
 import com.elta.android.data.features.user.mapper.ProfileToDomainMapper
@@ -64,10 +66,10 @@ import com.elta.android.domain.features.diary.events.model.Event
 import com.elta.android.domain.features.diary.tags.model.Tag
 import com.elta.android.domain.features.firmware.model.Firmware
 import com.elta.android.domain.features.firmware.model.FirmwareFile
+import com.elta.android.domain.features.observers.model.Observer
 import com.elta.android.domain.features.reminder.model.Reminder
 import com.elta.android.domain.features.sale_points.model.Coordinates
 import com.elta.android.domain.features.sale_points.model.SalePoint
-import com.elta.android.domain.features.observers.model.Observer
 import com.elta.android.domain.features.user.model.Profile
 import com.elta.android.domain.features.user.model.SocialNetwork
 import dagger.Binds
@@ -117,6 +119,11 @@ abstract class MappersModule {
     abstract fun bindEventFromCacheMapper(
         mapper: EventFromCacheMapper
     ): Mapper<EventCachedDto, EventDto>
+
+    @Binds
+    abstract fun bindEventFromGlucometerMapper(
+        mapper: EventFromGlucometerMapper
+    ): Mapper<GlucometerEventDto, Event>
 
     @Binds
     abstract fun bindEventToSimpleMapper(
