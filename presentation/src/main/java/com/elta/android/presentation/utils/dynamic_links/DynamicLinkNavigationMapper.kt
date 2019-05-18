a31a157064ab7845f1562ad668c35b8dd3139069
v.nykitchuk <v.nykitchuk@crysberry.com>
2019-05-19 02:04:30 +0300

    [ELTA-849] Add firebase InApp Messaging

--- presentation/src/main/java/com/elta/android/presentation/utils/dynamic_links/DynamicLinkNavigationMapper.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/dynamic_links/DynamicLinkNavigationMapper.kt
@@ -3,11 +3,13 @@ package com.elta.android.presentation.utils.dynamic_links
 import android.net.Uri
 import com.elta.android.presentation.Screens
 import ru.terrakok.cicerone.android.support.SupportAppScreen
+import timber.log.Timber
 
 object DynamicLinkNavigationMapper {
 
     private const val RESET_PASSWORD_PATH = "resetpassword"
     private const val VERIFY_EMAIL_PATH = "emailconfirmed"
+    private const val IN_APP_MESSAGE_PATH = "inappmessagelink"
     private const val QUERY_TOKEN = "token"
 
     fun deepLinkToScreen(uri: Uri): SupportAppScreen? {
@@ -17,6 +19,11 @@ object DynamicLinkNavigationMapper {
                 Screens.PasswordCreate(token)
             token != null && uri.lastPathSegment.equals(VERIFY_EMAIL_PATH, true) ->
                 Screens.EmailConfirmation(token)
+            // todo test inapp messaging
+            uri.lastPathSegment.equals(IN_APP_MESSAGE_PATH, true) -> {
+                Timber.e("deepLinkToScreen IN_APP_MESSAGE_PATH >> ")
+                Screens.HomeFlow
+            }
             else -> null
         }
     }
