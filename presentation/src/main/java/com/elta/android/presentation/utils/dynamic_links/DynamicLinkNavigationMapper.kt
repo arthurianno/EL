afc8186d62ea6e8368c0ef17efc6ef5857e3c33a
v.nykitchuk <v.nykitchuk@crysberry.com>
2019-05-21 02:24:27 +0300

    [ELTA-849] Update dynamic link mapper

--- presentation/src/main/java/com/elta/android/presentation/utils/dynamic_links/DynamicLinkNavigationMapper.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/dynamic_links/DynamicLinkNavigationMapper.kt
@@ -3,7 +3,6 @@ package com.elta.android.presentation.utils.dynamic_links
 import android.net.Uri
 import com.elta.android.presentation.Screens
 import ru.terrakok.cicerone.android.support.SupportAppScreen
-import timber.log.Timber
 
 object DynamicLinkNavigationMapper {
 
@@ -11,18 +10,24 @@ object DynamicLinkNavigationMapper {
     private const val VERIFY_EMAIL_PATH = "emailconfirmed"
     private const val IN_APP_MESSAGE_PATH = "inappmessagelink"
     private const val QUERY_TOKEN = "token"
+    private const val QUERY_SCREEN = "screen"
+    private const val SETTINGS_SCREEN = "settings"
 
     fun deepLinkToScreen(uri: Uri): SupportAppScreen? {
         val token = uri.getQueryParameter(QUERY_TOKEN)
+        val screen = uri.getQueryParameter(QUERY_SCREEN)
         return when {
             token != null && uri.lastPathSegment.equals(RESET_PASSWORD_PATH, true) ->
                 Screens.PasswordCreate(token)
             token != null && uri.lastPathSegment.equals(VERIFY_EMAIL_PATH, true) ->
                 Screens.EmailConfirmation(token)
-            // todo test inapp messaging
-            uri.lastPathSegment.equals(IN_APP_MESSAGE_PATH, true) -> {
-                Timber.e("deepLinkToScreen IN_APP_MESSAGE_PATH >> ")
-                Screens.HomeFlow
+            // todo test inapp messaging(temporally solution)
+            screen != null && uri.lastPathSegment.equals(IN_APP_MESSAGE_PATH, true) -> {
+                if (screen == SETTINGS_SCREEN) {
+                    Screens.ProfileSettings
+                } else {
+                    Screens.HomeFlow
+                }
             }
             else -> null
         }
