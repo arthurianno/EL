4d0c4611a7f140d326cb90107534d7935420e780
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-21 15:13:15 +0300

    [ELTA-1055] Implement checking for new firmware

--- presentation/src/main/java/com/elta/android/presentation/features/devices/firmware/pm/FirmwarePm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/devices/firmware/pm/FirmwarePm.kt
@@ -1,9 +1,141 @@
 package com.elta.android.presentation.features.devices.firmware.pm
 
+import com.elta.android.domain.features.devices.interactor.GetLastGlucometerInfoUseCase
+import com.elta.android.domain.features.devices.interactor.isFirmwareNewer
+import com.elta.android.domain.features.devices.model.GlucometerInfo
+import com.elta.android.domain.features.firmware.interactor.GetFirmwareInfoUseCase
+import com.elta.android.domain.features.firmware.model.Firmware
+import com.elta.android.presentation.R
 import com.elta.android.presentation.core.pm.BasePm
 import com.elta.android.presentation.core.pm.ServiceFacade
+import com.elta.android.presentation.core.ui.dialog.DialogData
+import com.elta.android.presentation.features.sync.control.bluetoothControl
+import com.nullgr.core.resources.ResourceProvider
+import io.reactivex.Observable
+import me.dmdev.rxpm.widget.dialogControl
 import javax.inject.Inject
 
 class FirmwarePm @Inject constructor(
+    private val getLastGlucometerInfoUseCase: GetLastGlucometerInfoUseCase,
+    private val getFirmwareInfoUseCase: GetFirmwareInfoUseCase,
     services: ServiceFacade
-) : BasePm(services)
+) : BasePm(services) {
+
+    val deviceAddressState = State<String>()
+    val deviceInfo = State<GlucometerInfo>()
+    val buttonAction = Action<Unit>()
+    val updateState = State<UpdateState>(UpdateState.Progress(resources))
+    val lowBatteryDialogControl = dialogControl<DialogData, Unit>()
+
+    val btControl = bluetoothControl()
+
+    private val getDeviceInfoAction = Action<String>()
+    private val checkUpdatesAction = Action<Unit>()
+    private val startUpdateAction = Action<Unit>()
+
+    override fun onCreate() {
+        super.onCreate()
+
+        buttonAction.observable
+            .filter { updateState.value is UpdateState.NotFound }
+            .subscribe(checkUpdatesAction.consumer)
+            .untilDestroy()
+
+        checkUpdatesAction.observable
+            .skipWhileInProgress()
+            .flatMapSingle {
+                getFirmwareInfoUseCase.execute()
+                    .bindProgress()
+                    .doOnSubscribe {
+                        updateState.consumer.accept(UpdateState.Progress(resources, deviceInfo.valueOrNull?.softwareVersion?.toString()))
+                    }
+                    .doOnSuccess(::handleFirmwareInfo)
+                    .doOnError(::handleError)
+            }
+            .retry()
+            .subscribe()
+            .untilDestroy()
+
+        getDeviceInfoAction.observable
+            .map(::createGetDeviceInfoUseCaseParams)
+            .flatMapSingle { params ->
+                getLastGlucometerInfoUseCase.execute(params)
+                    .doOnSuccess(::handleDeviceInfo)
+                    .doOnError(::handleError)
+            }
+            .retry()
+            .subscribe()
+            .untilDestroy()
+
+        Observable.merge(
+            buttonAction.observable.filter { updateState.value is UpdateState.Found },
+            btControl.bluetoothEnabledAction.observable,
+            btControl.locationPermissionsGrantedAction.observable,
+            btControl.locationEnabledAction.observable
+        )
+            .subscribe(startUpdateAction.consumer)
+            .untilDestroy()
+
+    }
+
+    fun setDeviceAddress(address: String) {
+        deviceAddressState.consumer.accept(address)
+        getDeviceInfoAction.consumer.accept(address)
+    }
+
+    private fun createGetDeviceInfoUseCaseParams(address: String): GetLastGlucometerInfoUseCase.Params =
+        GetLastGlucometerInfoUseCase.Params(address)
+
+    private fun handleDeviceInfo(info: GlucometerInfo) {
+        deviceInfo.consumer.accept(info)
+        updateState.consumer.accept(UpdateState.Progress(resources, info.softwareVersion?.toString()))
+        checkUpdatesAction.consumer.accept(Unit)
+    }
+
+    private fun handleFirmwareInfo(firmware: Firmware) {
+        deviceInfo.valueOrNull?.let {
+            val deviceVersionString = deviceInfo.valueOrNull?.softwareVersion?.toString() ?: "0"
+            if (it.isFirmwareNewer(firmware)) {
+                updateState.consumer.accept(UpdateState.Found(resources, firmware.version, deviceVersionString))
+            } else {
+                updateState.consumer.accept(UpdateState.NotFound(resources, deviceVersionString))
+            }
+        }
+    }
+
+    sealed class UpdateState {
+
+        abstract val title: String
+        abstract val version: String?
+        abstract val hint: String?
+        abstract val button: String?
+
+        data class Progress(
+            val resources: ResourceProvider,
+            val currentVersion: String? = null,
+            override val title: String = resources.getString(R.string.firmware_title_checking_updates),
+            override val version: String? = currentVersion?.let { resources.getString(R.string.firmware_version_current, it) },
+            override val hint: String? = null,
+            override val button: String? = null
+        ) : UpdateState()
+
+        data class NotFound(
+            val resources: ResourceProvider,
+            val currentVersion: String? = null,
+            override val title: String = resources.getString(R.string.firmware_title_updates_not_found),
+            override val version: String? = currentVersion?.let { resources.getString(R.string.firmware_version_current, it) },
+            override val hint: String? = null,
+            override val button: String? = resources.getString(R.string.firmware_button_check_updates)
+        ) : UpdateState()
+
+        data class Found(
+            val resources: ResourceProvider,
+            val newVersion: String,
+            val currentVersion: String? = null,
+            override val title: String = resources.getString(R.string.firmware_title_updates_found, newVersion),
+            override val version: String? = currentVersion?.let { resources.getString(R.string.firmware_version_current, it) },
+            override val hint: String? = resources.getString(R.string.firmware_updates_hint),
+            override val button: String? = resources.getString(R.string.firmware_button_update)
+        ) : UpdateState()
+    }
+}
--- presentation/src/main/java/com/elta/android/presentation/features/devices/firmware/ui/FirmwareFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/devices/firmware/ui/FirmwareFragment.kt
@@ -1,17 +1,41 @@
 package com.elta.android.presentation.features.devices.firmware.ui
 
+import android.os.Bundle
 import com.elta.android.presentation.R
 import com.elta.android.presentation.core.ui.fragment.BaseFragment
 import com.elta.android.presentation.core.ui.system_ui.LightStatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.features.devices.firmware.pm.FirmwarePm
 import com.elta.android.presentation.utils.bundle
+import com.jakewharton.rxbinding2.view.clicks
+import com.nullgr.core.ui.extensions.toggleView
+import kotlinx.android.synthetic.main.fragment_update_firmware.*
 
 class FirmwareFragment : BaseFragment<FirmwarePm>() {
 
     override val screenLayout: Int = R.layout.fragment_update_firmware
     override val classToken: Class<FirmwarePm> = FirmwarePm::class.java
     override val statusBarConfigProvider: StatusBarConfigProvider = LightStatusBarConfigProvider
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val address = arguments?.getString(EXTRA_ADDRESS)
+        address?.let { presentationModel.setDeviceAddress(address) }
+    }
+
+    override fun onBindPresentationModel(pm: FirmwarePm) {
+        super.onBindPresentationModel(pm)
+        actionButtonView.clicks().bindTo(pm.buttonAction)
+        pm.updateState.bindTo {
+            with(it) {
+                updateTitleView.text = title
+                updateVersionView.text = version
+                updateHintView.toggleView(hint != null)
+                actionButtonView.text = button
+                actionButtonView.toggleView(button != null)
+            }
+        }
+    }
 
     companion object {
         private const val EXTRA_ADDRESS = "extra_address"
