511ce84b5fd6b58f7a6f3c9fc26ef24f7b4b21ab
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-24 16:55:06 +0300

    [ELTA-1240] Add new ByTypeAndIds condition

--- data/src/main/java/com/elta/android/data/features/diary/events/cache/EventsConditions.kt
+++ data/src/main/java/com/elta/android/data/features/diary/events/cache/EventsConditions.kt
@@ -1,9 +1,11 @@
 package com.elta.android.data.features.diary.events.cache
 
 import com.elta.android.data.features.common.cache.Condition
+import com.elta.android.data.features.diary.events.dto.EventTypeDto
 import java.util.Date
 
 sealed class EventsConditions : Condition {
 
     data class ByPeriod(val start: Date, val end: Date) : EventsConditions()
+    data class ByTypeAndIds(val type: EventTypeDto, val ids: LongArray): EventsConditions()
 }
