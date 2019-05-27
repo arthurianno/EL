1109b144f5ed8a8e827550b2fe20087b51ed16f1
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-27 08:13:16 +0300

    [CodeStyle] Fix code style issues

--- data/src/main/java/com/elta/android/data/features/diary/events/cache/EventsConditions.kt
+++ data/src/main/java/com/elta/android/data/features/diary/events/cache/EventsConditions.kt
@@ -7,5 +7,5 @@ import java.util.Date
 sealed class EventsConditions : Condition {
 
     data class ByPeriod(val start: Date, val end: Date) : EventsConditions()
-    data class ByTypeAndIds(val type: EventTypeDto, val ids: LongArray): EventsConditions()
+    data class ByTypeAndIds(val type: EventTypeDto, val ids: LongArray) : EventsConditions()
 }
