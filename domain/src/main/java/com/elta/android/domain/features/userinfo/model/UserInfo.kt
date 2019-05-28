666a43a8fd15a3295412e85627c4899df53505b0
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-28 10:52:37 +0300

    [ELTA-658] Add documentation to UserInfo

--- domain/src/main/java/com/elta/android/domain/features/userinfo/model/UserInfo.kt
+++ domain/src/main/java/com/elta/android/domain/features/userinfo/model/UserInfo.kt
@@ -1,5 +1,11 @@
 package com.elta.android.domain.features.userinfo.model
 
+/**
+ * Represents local information about user.
+ *
+ * To update particular property of #UserInfo create new instance using named arguments.
+ * For example: to update #UserInfo.isFeedbackSent create UserInfo(isFeedbackSent = true).
+ */
 data class UserInfo(
     val isUserLoggedIn: Boolean? = null,
     var isFeedbackSent: Boolean? = null,
