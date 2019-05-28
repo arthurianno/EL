dac45ba5f17386a48c475e3e39c1cd5847e6672a
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-28 10:36:42 +0300

    [ELTA-658] Remove method isFeedbackWasSent

--- data/src/main/java/com/elta/android/data/features/feedback/repository/FeedbackDataRepository.kt
+++ data/src/main/java/com/elta/android/data/features/feedback/repository/FeedbackDataRepository.kt
@@ -2,19 +2,13 @@ package com.elta.android.data.features.feedback.repository
 
 import com.elta.android.data.features.feedback.datasource.FeedbackDataSource
 import com.elta.android.domain.features.feedback.repository.FeedbackRepository
-import com.elta.android.domain.features.userinfo.repository.UserInfoRepository
 import io.reactivex.Completable
-import io.reactivex.Single
 import javax.inject.Inject
 
 class FeedbackDataRepository @Inject constructor(
-    private val remoteSource: FeedbackDataSource,
-    private val userInfoRepository: UserInfoRepository
+    private val remoteSource: FeedbackDataSource
 ) : FeedbackRepository {
 
     override fun sendFeedback(name: String, email: String, message: String): Completable =
         remoteSource.sendFeedback(name, email, message)
-
-    override fun isFeedbackWasSent(): Single<Boolean> =
-        userInfoRepository.getUserInfo().map { it.isFeedbackSent }
 }
--- domain/src/main/java/com/elta/android/domain/features/feedback/repository/FeedbackRepository.kt
+++ domain/src/main/java/com/elta/android/domain/features/feedback/repository/FeedbackRepository.kt
@@ -1,11 +1,8 @@
 package com.elta.android.domain.features.feedback.repository
 
 import io.reactivex.Completable
-import io.reactivex.Single
 
 interface FeedbackRepository {
 
     fun sendFeedback(name: String, email: String, message: String): Completable
-
-    fun isFeedbackWasSent(): Single<Boolean>
 }
