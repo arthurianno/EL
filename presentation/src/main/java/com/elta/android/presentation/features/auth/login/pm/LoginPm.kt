bff716d375a156dd0e3a9e10434f2c8676e8b681
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-28 09:26:48 +0300

    [ELTA-658] Refactor LoginPm to use UserInfo instead of IsOnboardingPassedUseCase

--- presentation/src/main/java/com/elta/android/presentation/features/auth/login/pm/LoginPm.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/auth/login/pm/LoginPm.kt
@@ -3,8 +3,8 @@ package com.elta.android.presentation.features.auth.login.pm
 import com.elta.android.domain.features.auth.interactor.LoginUseCase
 import com.elta.android.domain.features.auth.interactor.LoginWithSocialNetworkUseCase
 import com.elta.android.domain.features.user.interactor.GetUserIdUseCase
-import com.elta.android.domain.features.user.interactor.IsOnboardingPassedUseCase
 import com.elta.android.domain.features.user.model.SocialNetworkType
+import com.elta.android.domain.features.userinfo.interactor.GetUserInfoUseCase
 import com.elta.android.presentation.Screens
 import com.elta.android.presentation.Screens.ActivateProfile
 import com.elta.android.presentation.analytics.model.AnalyticsEventParam
@@ -20,7 +20,7 @@ class LoginPm @Inject constructor(
     private val loginWithSocialNetworkUseCase: LoginWithSocialNetworkUseCase,
     private val loginUseCase: LoginUseCase,
     private val getUserIdUseCase: GetUserIdUseCase,
-    private val isOnboardingPassedUseCase: IsOnboardingPassedUseCase,
+    private val getUserInfoUseCase: GetUserInfoUseCase,
     services: ServiceFacade
 ) : BaseSocialPm(services) {
 
@@ -50,7 +50,7 @@ class LoginPm @Inject constructor(
                     .bindProgress()
                     .updateAnalyticStableParam()
                     .trackEvent(AnalyticsEventType.LOG_IN)
-                    .flatMap(::checkEmailAndOnboarding)
+                    .flatMap(::checkEmailAndOnBoarding)
                     .doOnError(::handleError)
             }
             .retry()
@@ -69,7 +69,7 @@ class LoginPm @Inject constructor(
                         AnalyticsEventType.LOG_IN,
                         AnalyticsEventParam.LOG_TYPE to params.network.name
                     )
-                    .flatMap(::checkEmailAndOnboarding)
+                    .flatMap(::checkEmailAndOnBoarding)
                     .doOnError(::handleError)
             }
             .retry()
@@ -90,20 +90,13 @@ class LoginPm @Inject constructor(
                 .map { isEmailActivated }
         }
 
-    private fun checkEmailAndOnboarding(isEmailActivated: Boolean) =
-        when (isEmailActivated) {
-            true -> checkIsOnboardingPassed()
-            else -> Single.just { Unit }
-                .doOnSuccess { router.navigateTo(ActivateProfile) }
-        }
-
-    private fun checkIsOnboardingPassed(): Single<Unit> =
-        isOnboardingPassedUseCase.execute()
-            .doOnSuccess { isOnboardingPassed ->
-                when (isOnboardingPassed) {
-                    true -> router.newRootFlow(Screens.HomeFlow)
+    private fun checkEmailAndOnBoarding(i: Boolean) =
+        getUserInfoUseCase.execute()
+            .doOnSuccess { info ->
+                when {
+                    !info.isEmailConfirmed -> router.navigateTo(ActivateProfile)
+                    info.isOnboardingPassed -> router.newRootFlow(Screens.HomeFlow)
                     else -> router.newRootFlow(Screens.OnBoardingFlow)
                 }
             }
-            .map { Unit }
 }
