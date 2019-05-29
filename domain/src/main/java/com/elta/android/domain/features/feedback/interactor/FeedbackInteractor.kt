7a6d09dbfbc1e4d6f04b3be08d1be800604c0d19
v.chernyshov <v.chernyshov@nullgr.com>
2019-05-29 16:54:01 +0300

    [ELTA-1147] Revert STEP_100 value

--- domain/src/main/java/com/elta/android/domain/features/feedback/interactor/FeedbackInteractor.kt
+++ domain/src/main/java/com/elta/android/domain/features/feedback/interactor/FeedbackInteractor.kt
@@ -5,7 +5,7 @@ import com.elta.android.domain.features.feedback.model.FeedbackDataModel
 const val NONE_STEP = -1
 const val STEP_10 = 10
 const val STEP_50 = 50
-const val STEP_100 = 2
+const val STEP_100 = 100
 
 fun getFeedbackModel(events: Int): FeedbackDataModel =
     when {
