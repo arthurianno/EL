2925d3fc20ed51d3082dee8b6a9449e9c1c4cf31
v.chernyshov <v.chernyshov@nullgr.com>
2019-06-04 13:43:17 +0300

    [ELTA-524] Clean code

--- common/src/main/java/com/elta/android/common/utils/DateExtensions.kt
+++ common/src/main/java/com/elta/android/common/utils/DateExtensions.kt
@@ -2,8 +2,6 @@ package com.elta.android.common.utils
 
 import org.threeten.bp.ZonedDateTime
 
-const val DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX"
-
 inline fun timestamp(): Long = ZonedDateTime.now().toMillis()
 
 inline fun ZonedDateTime?.isDateChanged(other: ZonedDateTime): Boolean {
