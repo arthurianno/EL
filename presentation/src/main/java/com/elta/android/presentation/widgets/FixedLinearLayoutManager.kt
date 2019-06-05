cdaac6e8352398f520210faa744cbca7f6b64ff3
v.chernyshov <v.chernyshov@nullgr.com>
2019-06-05 11:44:17 +0300

    [ELTA-1081] Add FixedLinearLayoutManager that wraps onLayoutChildren

---
+++ presentation/src/main/java/com/elta/android/presentation/widgets/FixedLinearLayoutManager.kt
@@ -0,0 +1,22 @@
+package com.elta.android.presentation.widgets
+
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import timber.log.Timber
+
+class FixedLinearLayoutManager(
+    context: Context,
+    orientation: Int = LinearLayoutManager.VERTICAL,
+    reverseLayout: Boolean = false
+) : LinearLayoutManager(context, orientation, reverseLayout) {
+
+    override fun onLayoutChildren(recycler: RecyclerView.Recycler?, state: RecyclerView.State?) {
+        try {
+            super.onLayoutChildren(recycler, state)
+        } catch (e: IndexOutOfBoundsException) {
+            // this is ugly solution to fix bug with inconsistency detected
+            Timber.i("Inconsistency detected. Invalid view holder adapter.")
+        }
+    }
+}
