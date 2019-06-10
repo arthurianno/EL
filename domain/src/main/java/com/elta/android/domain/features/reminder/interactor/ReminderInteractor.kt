9cb0041bf6336b751a236a5f8c5bc6be43a37624
v.chernyshov <v.chernyshov@nullgr.com>
2019-06-10 09:58:29 +0300

    [ELTA-1016] Add cases for missed one and more reminders

--- domain/src/main/java/com/elta/android/domain/features/reminder/interactor/ReminderInteractor.kt
+++ domain/src/main/java/com/elta/android/domain/features/reminder/interactor/ReminderInteractor.kt
@@ -6,17 +6,37 @@ import org.threeten.bp.ZonedDateTime
 
 fun List<Reminder>.sortByTime(): List<Reminder> = sortedBy { it.date }
 
-inline fun getNextReminderDate(type: ScheduleType, reminderDate: ZonedDateTime): ZonedDateTime? =
+inline fun getNextReminderDate(type: ScheduleType, reminderDate: ZonedDateTime, now: ZonedDateTime): ZonedDateTime? =
     when (type) {
         ScheduleType.NONE -> null
-        ScheduleType.DAY -> reminderDate.plusDays(1)
-        ScheduleType.WEEK -> reminderDate.plusDays(7)
-        ScheduleType.MONTH -> reminderDate.plusMonths(1)
-        ScheduleType.YEAR -> reminderDate.plusYears(1)
+        ScheduleType.DAY ->
+            reminderDate
+                .withYear(now.year)
+                .withMonth(now.monthValue)
+                .withDayOfMonth(now.dayOfMonth)
+                .plusDays(1)
+        ScheduleType.WEEK ->
+            reminderDate
+                .withYear(now.year)
+                .withMonth(now.monthValue)
+                .withDayOfMonth(now.dayOfMonth)
+                .plusDays(7)
+        ScheduleType.MONTH ->
+            reminderDate
+                .withYear(now.year)
+                .withMonth(now.monthValue)
+                .plusMonths(1)
+        ScheduleType.YEAR ->
+            reminderDate
+                .withYear(now.year)
+                .plusYears(1)
+    }?.apply {
+        withSecond(0)
+        withNano(0)
     }
 
 inline fun getNextReminder(reminder: Reminder): Reminder? =
-    getNextReminderDate(reminder.scheduleType, reminder.date)?.let { reminder.copy(date = it) }
+    getNextReminderDate(reminder.scheduleType, reminder.date, ZonedDateTime.now())?.let { reminder.copy(date = it) }
 
 inline fun Reminder.isNeedToUpdate(): Boolean =
     date.isBefore(ZonedDateTime.now()) && scheduleType != ScheduleType.NONE
--- domain/src/test/java/com/elta/android/domain/ReminderInteractorTest.kt
+++ domain/src/test/java/com/elta/android/domain/ReminderInteractorTest.kt
@@ -13,7 +13,8 @@ class ReminderInteractorTest {
     fun getNextReminderDate_ScheduleTypeNONE_null() {
         val type = ScheduleType.NONE
         val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
-        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate)
+        val now = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
         assert(nextDate == null)
     }
 
@@ -21,8 +22,19 @@ class ReminderInteractorTest {
     fun getNextReminderDate_ScheduleTypeDay_nextDay() {
         val type = ScheduleType.DAY
         val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
-        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate)
+        val now = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
         val nextExpectedDate = reminderDate.with(ChronoField.DAY_OF_MONTH, 7)
+        assert(nextDate == nextExpectedDate)
+    }
+
+    @Test
+    fun getNextReminderDate_ScheduleTypeDay_missedOneReminder_nextDay() {
+        val type = ScheduleType.DAY
+        val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val now = ZonedDateTime.of(2019, 6, 7, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
+        val nextExpectedDate = reminderDate.with(ChronoField.DAY_OF_MONTH, 8)
         assert(nextDate == nextExpectedDate)
     }
 
@@ -30,8 +42,19 @@ class ReminderInteractorTest {
     fun getNextReminderDate_ScheduleTypeWEEK_nextWeek() {
         val type = ScheduleType.WEEK
         val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
-        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate)
+        val now = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
         val nextExpectedDate = reminderDate.with(ChronoField.DAY_OF_MONTH, 13)
+        assert(nextDate == nextExpectedDate)
+    }
+
+    @Test
+    fun getNextReminderDate_ScheduleTypeWEEK_missedOneReminder_nextWeek() {
+        val type = ScheduleType.WEEK
+        val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val now = ZonedDateTime.of(2019, 6, 13, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
+        val nextExpectedDate = reminderDate.with(ChronoField.DAY_OF_MONTH, 20)
         assert(nextDate == nextExpectedDate)
     }
 
@@ -39,8 +62,19 @@ class ReminderInteractorTest {
     fun getNextReminderDate_ScheduleTypeMONTH_nextMonth() {
         val type = ScheduleType.MONTH
         val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
-        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate)
+        val now = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
         val nextExpectedDate = reminderDate.with(ChronoField.MONTH_OF_YEAR, 7)
+        assert(nextDate == nextExpectedDate)
+    }
+
+    @Test
+    fun getNextReminderDate_ScheduleTypeMONTH_missedOneReminder_nextMonth() {
+        val type = ScheduleType.MONTH
+        val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val now = ZonedDateTime.of(2019, 7, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
+        val nextExpectedDate = reminderDate.with(ChronoField.MONTH_OF_YEAR, 8)
         assert(nextDate == nextExpectedDate)
     }
 
@@ -48,8 +82,19 @@ class ReminderInteractorTest {
     fun getNextReminderDate_ScheduleTypeYEAR_nextYear() {
         val type = ScheduleType.YEAR
         val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
-        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate)
+        val now = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
         val nextExpectedDate = reminderDate.with(ChronoField.YEAR, 2020)
+        assert(nextDate == nextExpectedDate)
+    }
+
+    @Test
+    fun getNextReminderDate_ScheduleTypeYEAR_missedOneReminder_nextYear() {
+        val type = ScheduleType.YEAR
+        val reminderDate = ZonedDateTime.of(2019, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val now = ZonedDateTime.of(2020, 6, 6, 15, 23, 0, 0, ZoneId.systemDefault())
+        val nextDate = getNextReminderDate(type = type, reminderDate = reminderDate, now = now)
+        val nextExpectedDate = reminderDate.with(ChronoField.YEAR, 2021)
         assert(nextDate == nextExpectedDate)
     }
 }
