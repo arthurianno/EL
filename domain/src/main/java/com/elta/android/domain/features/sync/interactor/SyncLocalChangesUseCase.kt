ffc0b2f8bdae893a8627f378010297794dce8573
v.chernyshov <v.chernyshov@nullgr.com>
2019-06-26 17:23:59 +0300

    [ELTA-1426] Add InvalidRefreshToken error check to complete sync correctly

--- domain/src/main/java/com/elta/android/domain/features/sync/interactor/SyncLocalChangesUseCase.kt
+++ domain/src/main/java/com/elta/android/domain/features/sync/interactor/SyncLocalChangesUseCase.kt
@@ -1,5 +1,6 @@
 package com.elta.android.domain.features.sync.interactor
 
+import com.elta.android.common.errors.InvalidRefreshTokenError
 import com.elta.android.domain.features.diary.events.repository.EventsRepository
 import com.elta.android.domain.features.diary.tags.repository.TagsRepository
 import com.elta.android.domain.features.googlefit.repository.GoogleFitRepository
@@ -9,6 +10,7 @@ import com.nullgr.core.interactor.CompletableUseCase
 import com.nullgr.core.rx.applyScheduler
 import com.nullgr.core.rx.schedulers.SchedulersFacade
 import io.reactivex.Completable
+import io.reactivex.functions.Predicate
 import javax.inject.Inject
 
 class SyncLocalChangesUseCase @Inject constructor(
@@ -20,15 +22,17 @@ class SyncLocalChangesUseCase @Inject constructor(
     private val schedulers: SchedulersFacade
 ) : CompletableUseCase<Unit>(schedulers) {
 
+    private val predicate = Predicate<Throwable> { error -> error !is InvalidRefreshTokenError }
+
     override fun buildUseCaseObservable(params: Unit?): Completable =
         profileRepo.sync().applyScheduler(schedulers)
-            .onErrorComplete()
+            .onErrorComplete(predicate)
             .andThen(googleFitRepo.sync().applyScheduler(schedulers))
-            .onErrorComplete()
+            .onErrorComplete(predicate)
             .andThen(eventsRepo.sync().applyScheduler(schedulers))
-            .onErrorComplete()
+            .onErrorComplete(predicate)
             .andThen(tagsRepository.sync().applyScheduler(schedulers))
-            .onErrorComplete()
+            .onErrorComplete(predicate)
             .andThen(salePointsRepository.sync().applyScheduler(schedulers))
-            .onErrorComplete()
+            .onErrorComplete(predicate)
 }
