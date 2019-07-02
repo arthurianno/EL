c048e879d775d8ae2cba4fdf0e68da58eca6d781
v.chernyshov <v.chernyshov@nullgr.com>
2019-07-02 14:57:38 +0300

    [ELTA-1473] Add entry point for period chooser

--- presentation/src/main/java/com/elta/android/presentation/features/statistic/flow/ui/StatisticFlowFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/statistic/flow/ui/StatisticFlowFragment.kt
@@ -8,8 +8,11 @@ import com.elta.android.presentation.core.ui.fragment.BaseFlowFragment
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.TransparentStatusBarConfigProvider
 import com.elta.android.presentation.features.statistic.flow.pm.StatisticFlowPm
+import com.elta.android.presentation.features.statistic.report.ui.ReportPeriodChooserFragment
 import com.elta.android.presentation.utils.applyWindowInsetsForChildrenView
+import com.jakewharton.rxbinding2.view.clicks
 import com.nullgr.core.ui.extensions.hide
+import com.nullgr.core.ui.fragments.showDialog
 import kotlinx.android.synthetic.main.fragment_statistic_flow.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
@@ -23,7 +26,7 @@ class StatisticFlowFragment : BaseFlowFragment<StatisticFlowPm>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         homeButtonView.hide()
-        menuButtonView.hide()
+        menuButtonView.setText(R.string.statistic_menu_button)
         toolbarTitleView.setText(R.string.statistic_title)
         toolbarView.setBackgroundColor(ContextCompat.getColor(view.context, R.color.white))
         toolbarView.applyWindowInsetsForChildrenView()
@@ -33,6 +36,10 @@ class StatisticFlowFragment : BaseFlowFragment<StatisticFlowPm>() {
         super.onBindPresentationModel(pm)
         periodTabsView.tabClicks().bindTo(pm.periodSelectedAction)
         pm.selectedPeriodIdState.bindTo(periodTabsView.selection())
+        menuButtonView.clicks().bindTo(pm.menuAction)
+        pm.showReportPeriodChooser.bindTo {
+            childFragmentManager.showDialog(ReportPeriodChooserFragment.newInstance())
+        }
     }
 
     companion object {
