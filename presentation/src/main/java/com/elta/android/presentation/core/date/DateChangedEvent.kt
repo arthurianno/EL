1dd8b011a938c5873dcdb7170571377893fde394
v.chernyshov <v.chernyshov@nullgr.com>
2019-07-11 16:41:36 +0300

    [ELTA-1456] Rework DateChangeReceiver to support events handling without app launching

--- presentation/src/main/java/com/elta/android/presentation/core/date/DateChangedEvent.kt
+++ presentation/src/main/java/com/elta/android/presentation/core/date/DateChangedEvent.kt
@@ -1,6 +1,5 @@
 package com.elta.android.presentation.core.date
 
 import com.elta.android.presentation.core.bus.Event
-import org.threeten.bp.ZonedDateTime
 
-data class DateChangedEvent(val prevDate: ZonedDateTime, val newDate: ZonedDateTime) : Event
+object DateChangedEvent : Event
--- presentation/src/main/java/com/elta/android/presentation/di/ReceiverBuilder.kt
+++ presentation/src/main/java/com/elta/android/presentation/di/ReceiverBuilder.kt
@@ -1,6 +1,7 @@
 package com.elta.android.presentation.di
 
 import com.elta.android.common.di.scope.ReceiverScope
+import com.elta.android.presentation.core.date.DateChangeReceiver
 import com.elta.android.presentation.features.profile.settings.reminders.utils.BootReceiver
 import com.elta.android.presentation.features.profile.settings.reminders.utils.ReminderCallbackReceiver
 import dagger.Module
@@ -16,4 +17,8 @@ abstract class ReceiverBuilder {
     @ReceiverScope
     @ContributesAndroidInjector
     abstract fun bindReminderNotificationReceiver(): ReminderCallbackReceiver
+
+    @ReceiverScope
+    @ContributesAndroidInjector
+    abstract fun bindDateChangeReceiver(): DateChangeReceiver
 }
