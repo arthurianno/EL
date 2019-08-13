1b6806f7104849b9acb82bd750622c294baf4a87
v.chernyshov <v.chernyshov@nullgr.com>
2019-08-13 09:13:02 +0300

    [ELTA-1600] Add duration for snack bar with button

--- presentation/src/main/java/com/elta/android/presentation/messages/SnackBarMessageData.kt
+++ presentation/src/main/java/com/elta/android/presentation/messages/SnackBarMessageData.kt
@@ -7,12 +7,17 @@ import com.nullgr.core.resources.ResourceProvider
 sealed class SnackBarMessageData(
     override val icon: Int? = null,
     override val message: String,
-    override val button: String? = null
+    override val button: String? = null,
+    override val duration: Int? = null
 ) : SnackBarData {
 
     class SimpleTextMessage(message: String) : SnackBarMessageData(message = message)
 
-    class WithButton(message: String, button: String) : SnackBarMessageData(message = message, button = button)
+    class WithButton(
+        message: String,
+        button: String,
+        duration: Int? = null
+    ) : SnackBarMessageData(message = message, button = button, duration = duration)
 
     class ServerUnavailableMessage(resourceProvider: ResourceProvider) :
         SnackBarMessageData(
