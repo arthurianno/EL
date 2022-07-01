dd02237592dc6fb5829f711a04deee481bebe689
Sergey Aristov <aristov775@gmail.com>
2022-07-01 14:25:11 +0300

    Fix bugs on the observer view. Create ListAdapter for observer list

--- presentation/src/main/java/com/elta/android/presentation/features/observers/all/ui/ObserversFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/observers/all/ui/ObserversFragment.kt
@@ -2,20 +2,31 @@ package com.elta.android.presentation.features.observers.all.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.elta.android.presentation.R
-import com.elta.android.presentation.core.ui.fragment.BaseListFragment
+import com.elta.android.presentation.core.ui.fragment.BaseRecyclerViewFragment
 import com.elta.android.presentation.core.ui.system_ui.LightStatusBarConfigProvider
 import com.elta.android.presentation.core.ui.system_ui.StatusBarConfigProvider
 import com.elta.android.presentation.databinding.FragmentObserversBinding
 import com.elta.android.presentation.features.observers.all.pm.ObserversPm
+import com.elta.android.presentation.features.observers.all.ui.adapter.ObserverAdapter
 import com.jakewharton.rxbinding2.view.clicks
+import com.nullgr.core.adapter.items.ListItem
 import me.dmdev.rxpm.bindTo
+import javax.inject.Inject
 
 class ObserversFragment :
-    BaseListFragment<ObserversPm, FragmentObserversBinding>(FragmentObserversBinding::inflate) {
+    BaseRecyclerViewFragment<ObserversPm, FragmentObserversBinding>(FragmentObserversBinding::inflate) {
+
+    @Inject
+    lateinit var obseverAdapter: ObserverAdapter
+
+    override val adapter: ListAdapter<ListItem, RecyclerView.ViewHolder> by lazy { obseverAdapter }
 
     override val screenLayout: Int = R.layout.fragment_observers
     override val classToken: Class<ObserversPm> = ObserversPm::class.java
+
     override val statusBarConfigProvider: StatusBarConfigProvider = LightStatusBarConfigProvider
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
