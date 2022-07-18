1d236388dc16c203885e921bc5d737c056f53ed0
Sergey Aristov <aristov775@gmail.com>
2022-07-18 15:38:50 +0300

    Correct enter email (not more 64 chars on name before @)

--- domain/src/main/java/com/elta/android/domain/features/auth/interactor/AuthInteractor.kt
+++ domain/src/main/java/com/elta/android/domain/features/auth/interactor/AuthInteractor.kt
@@ -1,11 +1,16 @@
 package com.elta.android.domain.features.auth.interactor
 
-import android.util.Patterns
 import java.util.regex.Pattern
 
-val emailPattern: Pattern = Patterns.EMAIL_ADDRESS
+val emailPattern: Pattern =
+    Pattern.compile(
+        "^([a-z0-9_.-]{1,64}+)@([a-z0-9_.-]+)\\.([a-z.]{2,7})$",
+        Pattern.CASE_INSENSITIVE
+    )
+
 @Suppress("MaxLineLength")
-val passwordPattern: Pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+\$).{8,}\$")
+val passwordPattern: Pattern =
+    Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+\$).{8,}\$")
 
 fun isEmailValid(email: String): Boolean = emailPattern.matcher(email).matches()
 fun isPasswordValid(password: String): Boolean = passwordPattern.matcher(password).matches()
