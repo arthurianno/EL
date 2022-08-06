2e989a576b7f64973d3969509446313308a55bae
Ignat Mustafaev <tusxdie@gmail.com>
2022-08-06 13:21:40 +0700

    [BUGFIX][ELTA-606] Remarks fix

--- presentation/src/main/java/com/elta/android/presentation/features/observers/all/ui/adapter/holder/ObserverHeaderViewHolder.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/observers/all/ui/adapter/holder/ObserverHeaderViewHolder.kt
@@ -1,15 +1,15 @@
 package com.elta.android.presentation.features.observers.all.ui.adapter.holder
 
-import androidx.core.view.isVisible
 import com.elta.android.presentation.core.ui.adapter.BaseListItemViewHolder
 import com.elta.android.presentation.databinding.ItemProfileSettingsBinding
 import com.elta.android.presentation.features.observers.all.ui.adapter.items.ObserverHeaderItem
+import com.nullgr.core.ui.extensions.hide
 
 class ObserverHeaderViewHolder(
     private val binding: ItemProfileSettingsBinding
 ) : BaseListItemViewHolder<ObserverHeaderItem>(binding.root) {
     override fun bind(item: ObserverHeaderItem) {
         binding.settingsTitleView.text = item.title
-        binding.nextIconView.isVisible = false
+        binding.nextIconView.hide()
     }
 }
