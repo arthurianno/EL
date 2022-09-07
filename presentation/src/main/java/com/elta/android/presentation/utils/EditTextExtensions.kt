91c14d054c26bf115804248392f7bfd537366be9
Ignat Mustafaev <tusxdie@gmail.com>
2022-09-07 17:17:33 +0700

    [BUGFIX][EWS-423] No emoji!

--- presentation/src/main/java/com/elta/android/presentation/utils/EditTextExtensions.kt
+++ presentation/src/main/java/com/elta/android/presentation/utils/EditTextExtensions.kt
@@ -1,5 +1,6 @@
 package com.elta.android.presentation.utils
 
+import android.text.InputFilter
 import android.text.method.PasswordTransformationMethod
 import android.widget.EditText
 import androidx.appcompat.widget.AppCompatEditText
@@ -26,4 +27,14 @@ fun <E : AppCompatEditText> E.error(): Consumer<String> = Consumer {
 
 fun MaterialEditText.clearError() {
     error = null
+}
+
+fun EditText.setEmojiFilter() {
+    val emojiFilter = InputFilter { source, start, end, dest, dstart, dend ->
+        source.forEach {
+            if (it.isSurrogate()) return@InputFilter ""
+        }
+        return@InputFilter null
+    }
+    filters += emojiFilter
 }
