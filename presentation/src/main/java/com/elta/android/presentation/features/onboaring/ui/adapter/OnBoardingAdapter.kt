fc307e7d2107a432a9967cac6f3919c7ffa4e43f
Sergey Aristov <aristov775@gmail.com>
2022-09-08 07:46:28 +0300

    refactoring OnBoarding

---
+++ presentation/src/main/java/com/elta/android/presentation/features/onboaring/ui/adapter/OnBoardingAdapter.kt
@@ -0,0 +1,43 @@
+package com.elta.android.presentation.features.onboaring.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.elta.android.presentation.core.ui.adapter.BaseListAdapter
+import com.elta.android.presentation.databinding.ItemOnboardingDiabetesTypesBinding
+import com.elta.android.presentation.databinding.ItemOnboardingGenderBinding
+import com.elta.android.presentation.databinding.ItemOnboardingWeightBinding
+import com.elta.android.presentation.features.onboaring.ui.adapter.holder.OnBoardingDiabetesViewHolder
+import com.elta.android.presentation.features.onboaring.ui.adapter.holder.OnBoardingGenderViewHolder
+import com.elta.android.presentation.features.onboaring.ui.adapter.holder.OnBoardingWeightViewHolder
+import com.elta.android.presentation.features.onboaring.ui.adapter.items.OnBoardingDiabetesItem
+import com.elta.android.presentation.features.onboaring.ui.adapter.items.OnBoardingGenderItem
+import com.elta.android.presentation.features.onboaring.ui.adapter.items.OnBoardingWeightItem
+import com.nullgr.core.resources.ResourceProvider
+import com.nullgr.core.rx.RxBus
+import javax.inject.Inject
+
+class OnBoardingAdapter @Inject constructor(
+    private val bus: RxBus,
+    private val resources: ResourceProvider
+) : BaseListAdapter() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (viewType) {
+            OnBoardingGenderItem::class.java.hashCode() -> OnBoardingGenderViewHolder(
+                binding = ItemOnboardingGenderBinding.inflate(inflater, parent, false),
+                bus = bus
+            )
+            OnBoardingWeightItem::class.java.hashCode() -> OnBoardingWeightViewHolder(
+                binding = ItemOnboardingWeightBinding.inflate(inflater, parent, false),
+                bus = bus
+            )
+            OnBoardingDiabetesItem::class.java.hashCode() -> OnBoardingDiabetesViewHolder(
+                binding = ItemOnboardingDiabetesTypesBinding.inflate(inflater, parent, false),
+                bus = bus,
+                resources = resources
+            )
+            else -> throw IllegalArgumentException("No delegate defined for ${this::class.simpleName}")
+        }
+    }
+}
