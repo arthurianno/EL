73e85ee572d51f8c70c032c52862e925649c5659
Sergey Aristov <aristovsergey@siberian.pro>
2022-10-08 17:00:59 +0300

    fix save weight in the onboarding. Update version number

--- presentation/src/main/java/com/elta/android/presentation/features/onboaring/ui/adapter/holder/OnBoardingWeightViewHolder.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/onboaring/ui/adapter/holder/OnBoardingWeightViewHolder.kt
@@ -1,14 +1,18 @@
 package com.elta.android.presentation.features.onboaring.ui.adapter.holder
 
+import com.elta.android.presentation.Events
+import com.elta.android.presentation.core.bus.event
 import com.elta.android.presentation.core.ui.adapter.BaseListItemViewHolder
 import com.elta.android.presentation.databinding.ItemOnboardingWeightBinding
 import com.elta.android.presentation.features.onboaring.ui.adapter.items.OnBoardingWeightItem
+import com.nullgr.core.rx.RxBus
 import java.util.concurrent.TimeUnit
 
 private const val INTERVAL = 300L
 
 class OnBoardingWeightViewHolder(
-    private val binding: ItemOnboardingWeightBinding
+    private val binding: ItemOnboardingWeightBinding,
+    private val bus: RxBus
 ) : BaseListItemViewHolder<OnBoardingWeightItem>(binding.root) {
     override fun bind(item: OnBoardingWeightItem) {
         if (item.initialValue != null && item.weight == null) {
@@ -16,6 +20,9 @@ class OnBoardingWeightViewHolder(
         }
         binding.weightView.valueChanges().throttleLast(INTERVAL, TimeUnit.MILLISECONDS)
             .subscribe { newValue ->
+                item.weight?.run {
+                    bus.event(Events.OnBoardingPageSelected(item))
+                }
                 item.weight = newValue
             }
     }
