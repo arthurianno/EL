e356478119b9ce345f0bf5dac8ad1f65239e90ff
Sergey Aristov <aristovsergey@siberian.pro>
2022-10-22 08:34:25 +0300

    Refactoring gradle in module core-collections

---
+++ core-collections/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+
+    compileSdk = AppConfig.completeSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    lint {
+        abortOnError = false
+    }
+}
+
+configurations.all {
+    resolutionStrategy.force("org.jetbrains.kotlin:kotlin-stdlib:${Dependencies.Kotlin.version}")
+}
+
+kapt {
+    correctErrorTypes = true
+}
+
+dependencies {
+}
