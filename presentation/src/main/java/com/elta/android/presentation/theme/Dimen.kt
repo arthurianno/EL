01a401dbf6bff6cd31975b07f300cacb2605358a
Sergey Aristov <aristovsergey@siberian.pro>
2022-10-27 16:23:46 +0300

    Create base classes. Create Theme. Create LocalContentProvider

---
+++ presentation/src/main/java/com/elta/android/presentation/theme/Dimen.kt
@@ -0,0 +1,12 @@
+package com.elta.android.presentation.theme
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class EltaDimens(
+    val contentPadding: Dp
+)
+
+internal val eltaDimens = EltaDimens(
+    contentPadding = 16.dp
+)
