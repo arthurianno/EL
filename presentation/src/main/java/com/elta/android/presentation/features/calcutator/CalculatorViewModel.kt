f32a3cde7a638c39280e63dda81a9e63c4b676d1
Sergey Aristov <aristovsergey@siberian.pro>
2022-10-31 15:35:59 +0300

    Release ViewModel Inject to Fragment

--- presentation/src/main/java/com/elta/android/presentation/features/calcutator/CalculatorViewModel.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/calcutator/CalculatorViewModel.kt
@@ -1,28 +1,14 @@
 package com.elta.android.presentation.features.calcutator
 
-import com.elta.android.domain.features.user.interactor.GetProfileUseCase
 import com.elta.android.presentation.core.viewmodel.Action
 import com.elta.android.presentation.core.viewmodel.BaseViewModel
 import com.elta.android.presentation.core.viewmodel.Event
 import com.elta.android.presentation.features.calcutator.model.CalculatorAction
 import com.elta.android.presentation.features.calcutator.model.CalculatorState
-import kotlinx.coroutines.rx2.asFlow
 import javax.inject.Inject
 
-class CalculatorViewModel @Inject constructor(
-    private val getProfile: GetProfileUseCase
-) : BaseViewModel<CalculatorState, Event, CalculatorAction>(CalculatorState()) {
-
-    init {
-        launch {
-            getProfile.execute()
-                .toObservable()
-                .asFlow()
-                .collect {
-                    reduceState { state.value.copy(profile = it) }
-                }
-        }
-    }
+class CalculatorViewModel @Inject constructor() :
+    BaseViewModel<CalculatorState, Event, CalculatorAction>(CalculatorState()) {
 
     override fun reduceStateByAction(
         currentState: CalculatorState,
