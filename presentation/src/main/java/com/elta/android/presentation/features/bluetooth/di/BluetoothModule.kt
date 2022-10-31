7c030ddabe884ab20bbcb2ac0a6b78d0e5b1f515
Sergey Aristov <aristovsergey@siberian.pro>
2022-10-31 15:57:37 +0300

    delete unused classes on the DI

--- presentation/src/main/java/com/elta/android/presentation/features/bluetooth/di/BluetoothModule.kt
+++
@@ -1,6 +0,0 @@
-package com.elta.android.presentation.features.bluetooth.di
-
-import dagger.Module
-
-@Module
-class BluetoothModule
--- presentation/src/main/java/com/elta/android/presentation/features/profile/settings/global/di/ProfileSettingsModule.kt
+++
@@ -1,6 +0,0 @@
-package com.elta.android.presentation.features.profile.settings.global.di
-
-import dagger.Module
-
-@Module
-class ProfileSettingsModule
