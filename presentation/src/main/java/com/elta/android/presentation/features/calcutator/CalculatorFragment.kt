dd8fc4d24570301646eae4279642a1a797319889
Sergey Aristov <aristovsergey@siberian.pro>
2022-11-03 17:24:33 +0300

    correct remarks

--- presentation/src/main/java/com/elta/android/presentation/features/calcutator/CalculatorFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/calcutator/CalculatorFragment.kt
@@ -118,170 +118,170 @@ class CalculatorFragment(
             }
         }
     }
-}
 
-@Composable
-private fun showMainContent(
-    viewModel: CalculatorViewModel,
-    dishes: List<DishUi>
-) {
-    if (dishes.isEmpty()) {
-        viewModel.downButtonWidgetModel.disable()
-        EmptyContent()
-    } else {
-        viewModel.downButtonWidgetModel.enable()
-        CalculateDishes(dishes)
+    @Composable
+    private fun showMainContent(
+        viewModel: CalculatorViewModel,
+        dishes: List<DishUi>
+    ) {
+        if (dishes.isEmpty()) {
+            viewModel.downButtonWidgetModel.disable()
+            EmptyContent()
+        } else {
+            viewModel.downButtonWidgetModel.enable()
+            CalculateDishes(dishes)
+        }
     }
-}
 
-@Composable
-private fun showSearchView(
-    viewModel: CalculatorViewModel,
-    state: State<CalculatorState>
-) {
-    val searchFieldState = viewModel.searchFieldWidgetModel.state.collectAsState()
-    if (searchFieldState.value.text.isEmpty()) {
-        LastWords(
-            lastWords = state.value.lastWords,
-            onClick = viewModel::lastWordOnClick
-        )
-    } else {
-        FindingDishes(
-            findingDishes = state.value.findingDishes,
-            onClick = viewModel::dishOnClick
-        )
+    @Composable
+    private fun showSearchView(
+        viewModel: CalculatorViewModel,
+        state: State<CalculatorState>
+    ) {
+        val searchFieldState = viewModel.searchFieldWidgetModel.state.collectAsState()
+        if (searchFieldState.value.text.isEmpty()) {
+            LastWords(
+                lastWords = state.value.lastWords,
+                onClick = viewModel::lastWordOnClick
+            )
+        } else {
+            FindingDishes(
+                findingDishes = state.value.findingDishes,
+                onClick = viewModel::dishOnClick
+            )
+        }
     }
-}
 
-@Composable
-private fun FindingDishes(findingDishes: List<DishUi>, onClick: (dish: DishUi) -> Unit) {
-    GetLocalProperties { dimens, _, colors, shapes, _ ->
-        LazyColumn(verticalArrangement = Arrangement.spacedBy(dimens.dishCardVerticalSpace)) {
-            items(items = findingDishes) { dish ->
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clip(shapes.dishCard)
-                        .border(dimens.borderWidth, colors.shadeBlack3, shapes.dishCard)
-                        .padding(dimens.contentPadding),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    CardBody(dish)
-                    ButtonPlus(onClick = { onClick(dish) })
+    @Composable
+    private fun FindingDishes(findingDishes: List<DishUi>, onClick: (dish: DishUi) -> Unit) {
+        GetLocalProperties { dimens, _, colors, shapes, _ ->
+            LazyColumn(verticalArrangement = Arrangement.spacedBy(dimens.dishCardVerticalSpace)) {
+                items(items = findingDishes) { dish ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(shapes.dishCard)
+                            .border(dimens.borderWidth, colors.shadeBlack3, shapes.dishCard)
+                            .padding(dimens.contentPadding),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        CardBody(dish)
+                        ButtonPlus(onClick = { onClick(dish) })
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-private fun RowScope.CardBody(dish: DishUi) {
-    GetLocalProperties { dimens, _, colors, _, types ->
-        Column(
-            modifier = Modifier
-                .padding(end = dimens.contentPadding)
-                .weight(1f)
-        ) {
-            Box {
+    @Composable
+    private fun RowScope.CardBody(dish: DishUi) {
+        GetLocalProperties { dimens, _, colors, _, types ->
+            Column(
+                modifier = Modifier
+                    .padding(end = dimens.contentPadding)
+                    .weight(1f)
+            ) {
+                Box {
+                    Text(
+                        text = dish.name,
+                        style = types.title3,
+                        color = colors.blackBlue,
+                        modifier = Modifier.padding(end = dimens.bigDim)
+                    )
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_verify_dish),
+                        contentDescription = null,
+                        modifier = Modifier.align(Alignment.TopEnd)
+                    )
+                }
+                VSpacerVerySmall()
                 Text(
-                    text = dish.name,
-                    style = types.title3,
-                    color = colors.blackBlue,
-                    modifier = Modifier.padding(end = dimens.bigDim)
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.ic_verify_dish),
-                    contentDescription = null,
-                    modifier = Modifier.align(Alignment.TopEnd)
-                )
-            }
-            VSpacerVerySmall()
-            Text(
-                text = "${dish.rationCount} ${dish.ration}",
-                color = colors.shadeBlack1
-            )
-        }
-    }
-}
-
-@Composable
-private fun LastWords(lastWords: List<String>, onClick: (word: String) -> Unit) {
-    GetLocalProperties { dimens, _, _, _, _ ->
-        LazyColumn() {
-            items(items = lastWords) { word ->
-                Text(
-                    text = word,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { onClick(word) }
-                        .padding(vertical = dimens.lastWordVertical)
+                    text = "${dish.rationCount} ${dish.ration}",
+                    color = colors.shadeBlack1
                 )
             }
         }
     }
-}
 
-@Composable
-private fun CalculateDishes(dishes: List<DishUi>) {
-}
+    @Composable
+    private fun LastWords(lastWords: List<String>, onClick: (word: String) -> Unit) {
+        GetLocalProperties { dimens, _, _, _, _ ->
+            LazyColumn() {
+                items(items = lastWords) { word ->
+                    Text(
+                        text = word,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { onClick(word) }
+                            .padding(vertical = dimens.lastWordVertical)
+                    )
+                }
+            }
+        }
+    }
 
-@Composable
-private fun CalculatorTopBar(
-    appTopBarWidgetModel: BaseAppTopBarWidgetModel,
-    searchInFocus: Boolean
-) {
-    GetLocalProperties { _, _, colors, _, types ->
-        VerticallyAnimation(visualState = !searchInFocus, toUp = false) {
-            BaseAppTopBar(
-                widgetModel = appTopBarWidgetModel,
-                backgroundColor = colors.gOrangeB,
-                textStyle = types.h2,
-                textColor = colors.white,
-                startIcon = R.drawable.ic_back
+    @Composable
+    private fun CalculateDishes(dishes: List<DishUi>) {
+    }
+
+    @Composable
+    private fun CalculatorTopBar(
+        appTopBarWidgetModel: BaseAppTopBarWidgetModel,
+        searchInFocus: Boolean
+    ) {
+        GetLocalProperties { _, _, colors, _, types ->
+            VerticallyAnimation(visualState = !searchInFocus, toUp = false) {
+                BaseAppTopBar(
+                    widgetModel = appTopBarWidgetModel,
+                    backgroundColor = colors.gOrangeB,
+                    textStyle = types.h2,
+                    textColor = colors.white,
+                    startIcon = R.drawable.ic_back
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun HelpText(
+        viewModel: CalculatorViewModel,
+        searchInFocus: Boolean
+    ) {
+        GetLocalProperties { _, _, colors, _, _ ->
+            val state = viewModel.state.collectAsState()
+            val searchFieldState = viewModel.searchFieldWidgetModel.state.collectAsState()
+            Text(
+                text = if (!searchInFocus) {
+                    state.value.helpText
+                } else {
+                    stringResource(
+                        id = if (searchFieldState.value.text.isEmpty()) {
+                            R.string.calculator_last_search
+                        } else {
+                            R.string.calculator_search_result
+                        }
+                    )
+                },
+                color = colors.shadeBlack2
             )
         }
     }
-}
 
-@Composable
-private fun HelpText(
-    viewModel: CalculatorViewModel,
-    searchInFocus: Boolean
-) {
-    GetLocalProperties { _, _, colors, _, _ ->
-        val state = viewModel.state.collectAsState()
-        val searchFieldState = viewModel.searchFieldWidgetModel.state.collectAsState()
-        Text(
-            text = if (!searchInFocus) {
-                state.value.helpText
-            } else {
-                stringResource(
-                    id = if (searchFieldState.value.text.isEmpty()) {
-                        R.string.calculator_last_search
-                    } else {
-                        R.string.calculator_search_result
-                    }
+    @Composable
+    private fun EmptyContent() {
+        GetLocalProperties { dimens, _, colors, _, types ->
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.calculator_empty_list_text),
+                    style = types.body1.copy(color = colors.shadeBlack2),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.padding(dimens.contentPadding)
                 )
-            },
-            color = colors.shadeBlack2
-        )
-    }
-}
-
-@Composable
-private fun EmptyContent() {
-    GetLocalProperties { dimens, _, colors, _, types ->
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Text(
-                text = stringResource(id = R.string.calculator_empty_list_text),
-                style = types.body1.copy(color = colors.shadeBlack2),
-                textAlign = TextAlign.Center,
-                modifier = Modifier.padding(dimens.contentPadding)
-            )
+            }
         }
     }
 }
