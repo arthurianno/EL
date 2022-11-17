1cbe2825958156c55f3baa9e5ca4231a08c26628
Sergey Aristov <aristovsergey@siberian.pro>
2022-11-17 16:46:36 +0300

    fix tests error

--- domain/src/test/java/com/elta/android/domain/factory/EventTestFactory.kt
+++ domain/src/test/java/com/elta/android/domain/factory/EventTestFactory.kt
@@ -39,6 +39,7 @@ object EventTestFactory {
             medicament = insulin?.drug,
             type = type,
             state = State.CREATED,
-            glucometerSerialNumber = null
+            glucometerSerialNumber = null,
+            dishes = null
         )
 }
