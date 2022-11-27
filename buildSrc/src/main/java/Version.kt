d7147f515b94ac3c379a0480fa2c83fb92cf2b14
Sergey Aristov <aristovsergey@siberian.pro>
2022-11-27 21:52:30 +0300

    clean code.

--- buildSrc/src/main/java/Version.kt
+++ buildSrc/src/main/java/Version.kt
@@ -1,7 +1,8 @@
-
 import java.io.File
 import java.io.IOException
 import java.util.concurrent.TimeUnit
+
+private const val WAIT_EXECUTE_COMMAND_TIME = 1000L
 
 object Version {
     val versionCode: Int = 252
@@ -61,14 +62,13 @@ object Version {
 
     private fun commandExec(command: String, workingDir: File): String? {
         try {
-            val parts = command.split("\\s".toRegex())
-            val proc = ProcessBuilder(*parts.toTypedArray())
+            val proc = ProcessBuilder(*command.split("\\s".toRegex()).toTypedArray())
                 .directory(workingDir)
                 .redirectOutput(ProcessBuilder.Redirect.PIPE)
                 .redirectError(ProcessBuilder.Redirect.PIPE)
                 .start()
 
-            proc.waitFor(60, TimeUnit.MINUTES)
+            proc.waitFor(WAIT_EXECUTE_COMMAND_TIME, TimeUnit.MILLISECONDS)
             return proc.inputStream.bufferedReader().readText()
         } catch (e: IOException) {
             e.printStackTrace()
