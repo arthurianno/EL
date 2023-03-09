900afe900515a501fa110f0f7a9267d38250bc1e
Sergey Aristov <aristovsergey@siberian.pro>
2023-03-09 13:13:33 +0300

    Fix don't work sync with glucometer

--- presentation/src/main/java/com/elta/android/presentation/features/sync/connect/onboarding/ui/FromOnBoardingConnectDeviceFragment.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/sync/connect/onboarding/ui/FromOnBoardingConnectDeviceFragment.kt
@@ -3,10 +3,8 @@ package com.elta.android.presentation.features.sync.connect.onboarding.ui
 import com.elta.android.presentation.features.sync.connect.base.pm.ConnectDevicePm
 import com.elta.android.presentation.features.sync.connect.base.ui.ConnectDeviceFragment
 import com.elta.android.presentation.features.sync.connect.onboarding.pm.FromOnBoardingConnectDevicePm
-import com.nullgr.core.ui.extensions.children
 import com.nullgr.core.ui.extensions.hide
 import com.nullgr.core.ui.extensions.show
-import com.nullgr.core.ui.extensions.toggleView
 import me.dmdev.rxpm.bindTo
 
 class FromOnBoardingConnectDeviceFragment : ConnectDeviceFragment<FromOnBoardingConnectDevicePm>() {
@@ -21,13 +19,10 @@ class FromOnBoardingConnectDeviceFragment : ConnectDeviceFragment<FromOnBoarding
     override fun onBindPresentationModel(pm: FromOnBoardingConnectDevicePm) {
         super.onBindPresentationModel(pm)
         pm.mstate.bindTo { state ->
-            binding.syncStateContainerView.children().forEach { view ->
-                view.toggleView(state.getId() == view.id)
-                if (state == ConnectDevicePm.ViewState.SYNC_COMPLETED || state == ConnectDevicePm.ViewState.CONNECTED) {
-                    binding.toolbar.menuButtonView.hide()
-                } else {
-                    binding.toolbar.menuButtonView.show()
-                }
+            if (state == ConnectDevicePm.ViewState.SYNC_COMPLETED || state == ConnectDevicePm.ViewState.CONNECTED) {
+                binding.toolbar.menuButtonView.hide()
+            } else {
+                binding.toolbar.menuButtonView.show()
             }
         }
     }
