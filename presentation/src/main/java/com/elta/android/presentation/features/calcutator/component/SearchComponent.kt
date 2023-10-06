2467a37a1ed7ca08f6e5510bb31941c834d90a5e
Vladislav Groshev <groshev@siberian.pro>
2023-10-06 07:19:57 +0000

    [BUGFIX][EWS-3047] Fix text under icon

--- presentation/src/main/java/com/elta/android/presentation/features/calcutator/component/SearchComponent.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/calcutator/component/SearchComponent.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -23,11 +28,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.elta.android.presentation.R
+import com.elta.android.presentation.core.compose.widgets.HSpacerMedium
 import com.elta.android.presentation.core.compose.widgets.VSpacer
 import com.elta.android.presentation.core.compose.widgets.VSpacerMedium
 import com.elta.android.presentation.features.calcutator.model.DishUiEntity
@@ -48,7 +59,12 @@ internal fun FindingDishesWithFatSecret(
             val loadState = findingDishes.loadState
 
             when {
-                loadState.refresh is LoadState.Loading -> CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                loadState.refresh is LoadState.Loading -> CircularProgressIndicator(
+                    modifier = Modifier.align(
+                        Alignment.Center
+                    )
+                )
+
                 loadState.refresh is LoadState.Error -> SearchError()
                 loadState.refresh is LoadState.NotLoading && findingDishes.itemCount == 0 -> NotResult()
                 else -> {
@@ -115,8 +131,7 @@ private fun DishesItem(
     dishesClick: (DishUiEntity?) -> Unit
 ) {
     GetLocalProperties { dimens, _, colors, shapes, _ ->
-
-        Row(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(shapes.dishCard)
@@ -131,78 +146,128 @@ private fun DishesItem(
                 .padding(
                     horizontal = dimens.contentPadding,
                     vertical = dimens.halfMediumDim
-                ),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                )
         ) {
-            dish?.let { dish -> CardBody(dish) }
-            Image(
-                painter = painterResource(id = R.drawable.btn_plus),
-                contentDescription = null
-            )
+            dish?.let { dish ->
+                CardBody(dish, trailingIcon = {
+                    Image(
+                        painter = painterResource(id = R.drawable.btn_plus),
+                        contentDescription = null
+                    )
+                })
+            }
         }
     }
 }
 
 @Composable
-internal fun RowScope.CardBody(dish: DishUiEntity) {
-    GetLocalProperties { dimens, _, colors, _, types ->
+internal fun CardBody(dish: DishUiEntity, trailingIcon: @Composable (Modifier) -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(IntrinsicSize.Max),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Column(
-            modifier = Modifier
-                .padding(end = dimens.contentPadding)
-                .weight(1f)
+            modifier = Modifier.weight(1f),
+            verticalArrangement = Arrangement.SpaceBetween
         ) {
-            val brandNameExist = dish.brandName.isNotEmpty()
-            if (brandNameExist) {
-                Text(
-                    text = dish.brandName,
-                    style = types.textStyle2,
-                    color = colors.shadeBlack1,
-                    maxLines = TWO_LINES_COUNT,
-                    overflow = TextOverflow.Ellipsis
+            Brand(dish.brandName)
+            TitleDish(dish)
+            Spacer(
+                modifier = Modifier
+                    .height(16.dp)
+                    .weight(1f)
+            )
+            ServingInfo(dish)
+        }
+        HSpacerMedium()
+        trailingIcon(Modifier.fillMaxHeight())
+    }
+}
+
+@Composable
+private fun TitleDish(dish: DishUiEntity) {
+    GetLocalProperties { dimens, _, colors, _, types ->
+        val iconId = "iconId"
+        val inlineContext = mapOf(
+            iconId to InlineTextContent(
+                placeholder = Placeholder(
+                    width = 20.sp,
+                    height = 20.sp,
+                    placeholderVerticalAlign = PlaceholderVerticalAlign.Center
                 )
-                VSpacer(height = dimens.smallestDim)
-            }
-            Row {
-                if (dish.isVerified) {
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_verify_dish),
-                        modifier = Modifier.padding(end = dimens.verySmallDim),
-                        contentDescription = null
-                    )
-                }
-                Text(
-                    text = dish.name,
-                    style = types.title3,
-                    color = colors.blackBlue,
-                    maxLines = THREE_LINES_COUNT,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
-            VSpacer(height = dimens.halfMediumDim)
-            Row(
-                modifier = Modifier.fillMaxWidth()
             ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_verify_dish),
+                    modifier = Modifier.padding(
+                        end = dimens.verySmallDim,
+                        top = dimens.smallestDim
+                    ),
+                    contentDescription = null
+                )
+            })
+        Text(
+            text = buildAnnotatedString {
+                if (dish.isVerified) {
+                    appendInlineContent(id = iconId)
+                }
+                append(text = dish.name)
+            },
+            inlineContent = inlineContext,
+            style = types.title3,
+            color = colors.blackBlue,
+            maxLines = THREE_LINES_COUNT,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Composable
+private fun Brand(name: String) {
+    GetLocalProperties { dimens, _, colors, _, types ->
+        if (name.isNotEmpty()) {
+            Text(
+                text = name,
+                style = types.textStyle2,
+                color = colors.shadeBlack1,
+                maxLines = TWO_LINES_COUNT,
+                overflow = TextOverflow.Ellipsis
+            )
+            VSpacer(height = dimens.smallestDim)
+        }
+    }
+}
+
+@Composable
+private fun ServingInfo(dish: DishUiEntity) {
+    GetLocalProperties { _, _, colors, _, types ->
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier.weight(weight = 1F, fill = false),
+                text = dish.servingCalories.first,
+                style = types.textStyle2,
+                color = colors.shadeBlack1,
+                maxLines = SINGLE_LINE_COUNT,
+                overflow = TextOverflow.Ellipsis
+            )
+            if (dish.servingCalories.second.isNotEmpty()) {
                 Text(
-                    modifier = Modifier.weight(weight = 1F, fill = false),
-                    text = dish.servingCalories.first,
+                    text = stringResource(
+                        id = R.string.calculator_dish_calories_in_serving,
+                        dish.servingCalories.second
+                    ),
                     style = types.textStyle2,
                     color = colors.shadeBlack1,
-                    maxLines = SINGLE_LINE_COUNT,
-                    overflow = TextOverflow.Ellipsis
+                    maxLines = SINGLE_LINE_COUNT
                 )
-                if (dish.servingCalories.second.isNotEmpty()){
-                    Text(
-                        text = stringResource(id = R.string.calculator_dish_calories_in_serving, dish.servingCalories.second),
-                        style = types.textStyle2,
-                        color = colors.shadeBlack1,
-                        maxLines = SINGLE_LINE_COUNT
-                    )
-                }
             }
         }
     }
 }
+
 @Composable
 private fun BoxScope.NotResult() {
     GetLocalProperties { _, _, colors, _, _ ->
