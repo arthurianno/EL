844946a3feca57d47b15c1b4f68cd7fafb3bebf8
Arthur <sitikovartur32@gmail.com>
2026-08-25 10:41:43 +0300

    fix: remove unnecessary navigation bars padding from GlucoseDashboardScreen

--- presentation/src/main/java/com/elta/android/presentation/features/main/records/ui/compose/GlucoseDashboardScreen.kt
+++ presentation/src/main/java/com/elta/android/presentation/features/main/records/ui/compose/GlucoseDashboardScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -156,7 +155,6 @@ fun GlucoseDashboardScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .navigationBarsPadding()
     ) {
         Column(
             modifier = Modifier
